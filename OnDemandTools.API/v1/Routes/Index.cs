535801e3cfe31f91a565613c3f1427965a0b53c6
Jason <jason.john@turner.com>
2016-12-18 19:20:02 -0500

    Added static indiex page. Work in progress

---
+++ OnDemandTools.API/v1/Routes/Index.cs
@@ -0,0 +1,19 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnDemandTools.API.v1.Routes
+{
+    public class Index: NancyModule       
+    {
+        public Index() : base("v1")
+        {
+            Get("/", _ =>
+            {
+                return Response.AsFile("Content/index.html", "text/html");
+            });
+        }
+    }
+}
