cdbec0c49050d237b642e4fca2d7dafb077e5c2a
Jason <jason.john@turner.com>
2016-12-19 19:55:22 -0500

    tryout simple asp.net project

--- OnDemandTools.API/Program.cs
+++ OnDemandTools.API/Program.cs
@@ -1,29 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+
 namespace OnDemandTools.API
 {
-    using System.IO;
-    using Microsoft.AspNetCore.Builder;
-    using Microsoft.AspNetCore.Hosting;
-    using Microsoft.Extensions.Configuration;
-
     public class Program
     {
-        /// <summary>
-        /// Main execution entry point
-        /// </summary>
-        /// <param name="args"></param>
         public static void Main(string[] args)
         {
             var config = new ConfigurationBuilder()
-                        .SetBasePath(Directory.GetCurrentDirectory())
-                        .AddJsonFile("hosting.json", optional: true)
-                        .Build();
+                .AddCommandLine(args)
+                .AddEnvironmentVariables(prefix: "ASPNETCORE_")
+                .Build();
 
-
-            // Host the service
             var host = new WebHostBuilder()
                 .UseConfiguration(config)
+                .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseKestrel()               
+                .UseIISIntegration()
                 .UseStartup<Startup>()
                 .Build();
 
--- OnDemandTools.API/Startup.cs
+++ OnDemandTools.API/Startup.cs
@@ -1,36 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
 namespace OnDemandTools.API
 {
-    using Microsoft.AspNetCore.Builder;
-    using Microsoft.AspNetCore.Hosting;
-    using Microsoft.Extensions.Configuration;
-    using Microsoft.Extensions.DependencyInjection;
-    using Microsoft.Extensions.Logging;
-    using Nancy.Owin;
-
-    /// <summary>
-    /// Defining start up configuration
-    /// </summary>
     public class Startup
     {
-        public IConfigurationRoot Configuration { get; set; }
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            // All dependency injection will be done in APIBootstrapper
+            // Add framework services.
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-           
-          
+            //loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            //loggerFactory.AddDebug();
 
-            // Specify request pipeline--strictly Nancy middleware
-            app.UseOwin(x => x.UseNancy()
-                );
+            app.UseMvc();
         }
-
     }
 }
