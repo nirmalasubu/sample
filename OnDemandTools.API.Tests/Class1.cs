4c83735f00454f6f399fcfee617b924581befc10
jjohn <dormantroot@gmail.com>
2017-01-13 17:16:07 -0500

    completing unit test shell

--- OnDemandTools.API.Tests/Class1.cs
+++ OnDemandTools.API.Tests/Class1.cs
@@ -12,11 +12,6 @@ namespace OnDemandTools.API.Tests
             Assert.Equal(4, Add(2, 2));
         }
 
-        [Fact]
-        public void FailingTest()
-        {
-            Assert.Equal(5, Add(2, 2));
-        }
 
         int Add(int x, int y)
         {
--- OnDemandTools.API.Tests/validate.sh
+++ OnDemandTools.API.Tests/validate.sh
@@ -1,9 +1,7 @@
-﻿#!/bin/bash
+#!/bin/bash
 containerName="$(docker ps --filter="name=api-test" -a --format '{{.Names}}')"
-echo $containerName
 
 containerStatus="$(docker inspect -f {{.State.ExitCode}} $containerName)"
-echo $containerStatus
 if [ "$containerStatus" != "0" ];
 then
   exit 1
