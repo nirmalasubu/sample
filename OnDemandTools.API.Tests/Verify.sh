0a188c84e7b0212cbd58080fcb799ee262bc3bc2
Jason <jason.john@turner.com>
2017-01-13 16:29:15 -0500

    more changes

--- OnDemandTools.API.Tests/Verify.sh
+++ OnDemandTools.API.Tests/Verify.sh
@@ -1,3 +1,11 @@
-﻿#!/bin/bash
+﻿containerName="$(docker ps --filter="name=api-test" -a --format '{{.Names}}')"
+echo $containerName
 
-echo "hi from verify.sh"
+containerStatus="$(docker inspect -f {{.State.ExitCode}} $containerName)"
+echo $containerStatus
+if [ "$containerStatus" != "0" ];
+then
+  exit 1
+else
+  exit 0
+fi	
