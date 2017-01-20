d73149068a4d87da3fd9e4223f9a8f7b62f8fe53
Jason <jason.john@turner.com>
2017-01-20 07:46:39 -0500

    unit wip

--- OnDemandTools.API.Tests/Class1.cs
+++ OnDemandTools.API.Tests/Class1.cs
@@ -1,5 +1,6 @@
 using RestSharp;
 using System;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -10,16 +11,44 @@ namespace OnDemandTools.API.Tests
     public class Class1
     {
         [Fact]
-        public void PassingTest()
+        public async void PassingTest()
         {
-            RestClient client = new RestClient("http://localhost:5000/whoami");
-            var request = new RestRequest(Method.GET);
-            Task.Run(async () =>
-            {
-                var rs = await GetHostingProviderDetails(client, request) as String;
-                var kk = 4;
 
-            }).Wait();
+            //RestClient client = new RestClient("http://localhost:5000/whoami");
+
+            //var request = new RestRequest(Method.GET);
+            //Task.Run(async () =>
+            //{
+            //    //var rs = await GetHostingProviderDetails(client, request) as String;
+            //    //var kk = 4;
+
+            //    await 
+
+            //}).Wait();
+
+
+            var httpClient = new HttpClient();
+            Task<string> contentsTask = httpClient.GetStringAsync("http://localhost:5000/whoami");
+            string contents = await contentsTask;
+
+            Assert.False(true);
+            //await Task.Run(async () =>
+            //{
+            //    client.ExecuteAsync(request, response =>
+            //    {
+            //        if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            //        {
+            //            var k = response.Content;
+            //        }
+            //        else
+            //        {
+            //            var k = String.Empty;
+            //        }
+            //    });
+            //}).Wait();
+
+
+
         }
 
         private Task<String> GetHostingProviderDetails(RestClient theClient, RestRequest theRequest)
