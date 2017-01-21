bc6c844be7d05d347a059ef03567ca25ac22b019
jjohn <dormantroot@gmail.com>
2017-01-21 01:53:51 -0500

    more changes

--- OnDemandTools.API.Tests/validate.sh
+++ OnDemandTools.API.Tests/validate.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-containerName="$(docker ps --filter="name=api-test" -a --format '{{.Names}}')"
-
-containerStatus="$(docker inspect -f {{.State.ExitCode}} $containerName)"
-if [ "$containerStatus" != "0" ];
-then
-  exit 1
-else
-  exit 0
-fi	
+curl -v http://localhost:5000/healthcheck
