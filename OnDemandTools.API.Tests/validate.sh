51d7c7da22cfdeea4111cd3f959aa9b45003d296
jjohn <dormantroot@gmail.com>
2017-01-21 02:52:30 -0500

    changes to circle

--- OnDemandTools.API.Tests/validate.sh
+++ OnDemandTools.API.Tests/validate.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-cat /etc/hosts
-curl -v http://odtapi:5000/healthcheck
+containerName="$(docker ps --filter="name=api-test" -a --format '{{.Names}}')"
+
+containerStatus="$(docker inspect -f {{.State.ExitCode}} $containerName)"
+if [ "$containerStatus" != "0" ];
+then
+  exit 1
+else
+  exit 0
+fi
