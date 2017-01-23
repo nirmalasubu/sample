c9cb1f50a9b7ae477c9a77b90421103edeb9e47c
c-hclent-ssoundarara <Sureshkumar.Soundararajan@turner.com>
2017-01-23 07:10:06 -0500

    neww file for message

---
+++ OnDemandTools.API/v1/Models/Airing/Long/AiringMessage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OnDemandTools.API.v1.Models.Airing.Long
+{
+    public class AiringMessage
+    {
+        public string AiringId { get; set; }
+       
+        public string Message { get; set; }
+    }
+}
