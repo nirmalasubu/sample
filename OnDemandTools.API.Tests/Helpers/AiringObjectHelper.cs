6110d07a03a2d97ffcb57d9edb5358f57207bb9e
Nirmala Subramaniam <nirmalasubu@yahoo.com>
2017-01-26 21:27:40 -0500

    ODTD-439 clean up

--- OnDemandTools.API.Tests/Helpers/AiringObjectHelper.cs
+++ OnDemandTools.API.Tests/Helpers/AiringObjectHelper.cs
@@ -1,48 +1,33 @@
 ﻿using Newtonsoft.Json.Linq;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace OnDemandTools.API.Tests.Helpers
 {
     public class AiringObjectHelper
     {
-       
-
-        public AiringObjectHelper()
-        {
-
-        }
         public string UpdateAiringId(string airingId,string jsonString)
         {
-
             JObject jsonObject = JObject.Parse(jsonString);
            
             jsonObject.Add("AiringId",airingId);
        
             return jsonObject.ToString();
-
         }
-
-      
-
+       
         public string UpdateDates(string jsonString, int noOfDaysBefore)
         {
-            JObject s = JObject.Parse(jsonString);
+            JObject jObject = JObject.Parse(jsonString);
 
-            JArray j = (JArray)s.SelectToken("Flights");
+            JArray jArray = (JArray)jObject.SelectToken("Flights");
            
-
-            foreach (JObject obj in j)
+            foreach (JObject obj in jArray)
             {
-              
                 obj["Start"] = DateTime.UtcNow.AddDays(noOfDaysBefore);
                 obj["End"] = DateTime.UtcNow.AddDays(noOfDaysBefore).AddDays(7);
 
                 noOfDaysBefore = noOfDaysBefore + 7;
             }
-            return s.ToString();
+            return jObject.ToString();
            
         }
     }
