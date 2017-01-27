deb577407aae4d11e5c9aaa3dfe8a2d75bce9454
c-hclent-ssoundarara <Sureshkumar.Soundararajan@turner.com>
2017-01-27 06:52:09 -0500

    ODT-434 Test get airing with destination brand dates

---
+++ OnDemandTools.API.Tests/AiringRoute/GetAiringByDestinationRule.cs
@@ -0,0 +1,62 @@
+﻿using Newtonsoft.Json.Linq;
+using OnDemandTools.API.Tests.Helpers;
+using RestSharp;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+
+namespace OnDemandTools.API.Tests.AiringRoute
+{
+    [TestCaseOrderer("OnDemandTools.API.Tests.Helpers.CustomTestCaseOrderer", "OnDemandTools.API.Tests")]
+    [Collection("API Collection")]
+    public class GetAiringByDestinationRules
+    {
+        APITestFixture fixture;
+        RestClient client;
+        public GetAiringByDestinationRules(APITestFixture fixture)
+        {
+            this.fixture = fixture;
+            this.client = this.fixture.restClient;
+        }
+
+        [Fact, Order(1)]
+        public void GetAiringByDestination_PassingWithInValidDestinationTest()
+        {
+            JArray response = default(JArray);
+            var request = new RestRequest("/v1/airings/destination/CMA", Method.GET);
+            Task.Run(async () =>
+            {
+                response = await client.RetrieveRecords(request);
+
+            }).Wait();
+
+            if (response.Count > 0)
+            {
+                var my_obj = response.First.Value<JArray>(@"flights").First.Value<JArray>(@"destinations").ToList();
+                var message = my_obj.Any(i => i.Value<String>(@"name") == "CMA") ? "Test Passed" : "User do not have access to destination";
+                Assert.True(!my_obj.Any(i => i.Value<String>(@"name") == "CMA"), message);
+            }
+        }
+
+        [Fact, Order(2)]
+        public void GetAiring_WithValidDestinationTest()
+        {
+            JArray response = default(JArray);
+            var request = new RestRequest("/v1/airings/destination/CNWB", Method.GET);
+            Task.Run(async () =>
+            {
+                response = await client.RetrieveRecords(request);
+
+            }).Wait();
+
+            if (response.Count > 0)
+            {
+                var my_obj = response.First.Value<JArray>(@"flights").First.Value<JArray>(@"destinations").ToList();
+                var message = my_obj.Any(i => i.Value<String>(@"name") == "CMA") ? "Test Passed" : "User do not have access to destination";
+                Assert.True(my_obj.Any(i => i.Value<String>(@"name") == "CMA"), message);
+            }
+        }
+    }
+}
---
+++ OnDemandTools.API.Tests/AiringRoute/GetAiringByFlightWindowRule.cs
@@ -0,0 +1,63 @@
+﻿using Newtonsoft.Json.Linq;
+using OnDemandTools.API.Tests.Helpers;
+using RestSharp;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+
+namespace OnDemandTools.API.Tests.AiringRoute
+{
+    [TestCaseOrderer("OnDemandTools.API.Tests.Helpers.CustomTestCaseOrderer", "OnDemandTools.API.Tests")]
+    [Collection("API Collection")]
+    public class GetAiringByFlightWindowRules
+    {
+        APITestFixture fixture;
+        RestClient client;
+        public GetAiringByFlightWindowRules(APITestFixture fixture)
+        {
+            this.fixture = fixture;
+            this.client = this.fixture.restClient;
+        }
+
+        [Fact, Order(1)]
+        public void GetAiringByBrandFlightWindowDetails_PassingWithValidBrandAndStartEndDate_PassTest()
+        {
+            JArray response = default(JArray);
+            var request = new RestRequest("/v1/airings/brand/Cartoon/destination/CMA?startDate=01/10/2020&endDate=01/18/2020", Method.GET);
+            Task.Run(async () =>
+            {
+                response = await client.RetrieveRecords(request);
+
+            }).Wait();
+
+            if (response.Count > 0)
+            {
+                var my_obj = response.First.Value<JArray>(@"flights").First;
+
+                Assert.True(response.First.Value<string>(@"brand") == "Cartoon", string.Format("Brand should be 'Cartoon' and but the returned {0}", response.First.Value<string>(@"brand")));
+                Assert.True(my_obj.Value<string>(@"start") == "01/10/2020 08:00:00", string.Format("Start should be '01/10/2020 08:00:00' and but the returned {0}", my_obj.Value<string>(@"start")));
+                Assert.True(my_obj.Value<string>(@"end") == "01/18/2020 07:59:59", string.Format("End should be '01/18/2020 07:59:59' and but the returned {0}", my_obj.Value<string>(@"end")));
+            }
+        }
+
+        [Fact, Order(2)]
+        public void GetAiringByBrandFlightWindowDetails_PassingWithInValid_StartDate_ErrorTest()
+        {
+            JObject response = default(JObject);
+            var request = new RestRequest("/v1/airings/brand/Cartoon/destination/CMA?startDate=18/01/2020&endDate=01/18/2020", Method.GET);
+            Task.Run(async () =>
+            {
+                response = await client.RetrieveRecord(request);
+
+            }).Wait();
+
+            string value = response.Value<string>(@"StatusCode");
+            if (value != null)
+            {
+                Assert.True(true, "message: String was not recognized as a valid DateTime.");
+            }
+        }
+    }
+}
