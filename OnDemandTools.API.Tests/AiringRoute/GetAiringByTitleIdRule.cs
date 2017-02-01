46ceecd653d45f3847b39a9da799d7853d044be4
c-hclent-ssoundarara <Sureshkumar.Soundararajan@turner.com>
2017-02-01 07:00:06 -0500

    ODT-438 & 450 Get airing by titleId and media Id generation test cases

---
+++ OnDemandTools.API.Tests/AiringRoute/GetAiringByTitleIdRule.cs
@@ -0,0 +1,61 @@
+﻿using Newtonsoft.Json.Linq;
+using OnDemandTools.API.Tests.Helpers;
+using RestSharp;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+
+namespace OnDemandTools.API.Tests.AiringRoute
+{
+    [TestCaseOrderer("OnDemandTools.API.Tests.Helpers.CustomTestCaseOrderer", "OnDemandTools.API.Tests")]
+    [Collection("API Collection")]
+    public class GetAiringByTitleIdRule
+    {
+        APITestFixture fixture;
+        RestClient client;
+        public GetAiringByTitleIdRule(APITestFixture fixture)
+        {
+            this.fixture = fixture;
+            this.client = this.fixture.restClient;
+        }
+
+        [Fact]
+        public void GetAiringByTitleId_PassingWithValidId()
+        {
+
+            JArray response = new JArray();
+            var request = new RestRequest("/v1/airings/titleId/2065580", Method.GET);
+            Task.Run(async () =>
+            {
+                response = await client.RetrieveRecords(request);
+
+            }).Wait();
+
+            string value = response.First.Value<String>(@"StatusCode");
+            if (value != null)
+            {
+                Assert.True(false, "TitleId : 2065580 is has no airings");
+            }
+
+            JArray jTitleIds = response.First.SelectToken("title").Value<JArray>(@"titleIds");
+            // Assert
+            Assert.True(jTitleIds.First.Value<string>(@"value") == "2065580", string.Format("Title Id should be '2065580' and but the returned {0}", jTitleIds.First.Value<string>(@"value")));
+
+        }
+
+        [Fact]
+        public void GetAiringByTitleId_PassingWithInValidId()
+        {
+            JArray response = new JArray();
+            var request = new RestRequest("/v1/airings/titleId/1111", Method.GET);
+            Task.Run(async () =>
+            {
+                response = await client.RetrieveRecords(request);
+
+            }).Wait();
+
+            Assert.True((response.First == null), "1111 is has no airings");
+        }
+    }
+}
---
+++ OnDemandTools.API.Tests/AiringRoute/PostAiring/MediaIdGenerationRule.cs
@@ -0,0 +1,68 @@
+﻿using Newtonsoft.Json.Linq;
+using OnDemandTools.API.Tests.Helpers;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace OnDemandTools.API.Tests.AiringRoute.PostAiring
+{
+    /// <summary>
+    /// Hint : order 1 runs first and  then order 2 runs
+    /// </summary>
+    [TestCaseOrderer("OnDemandTools.API.Tests.Helpers.CustomTestCaseOrderer", "OnDemandTools.API.Tests")]
+    [Collection("API Collection")]
+    public class MediaIdGenerationRule : BaseAiringRule
+    {
+        private readonly AiringObjectHelper _airingObjectHelper;
+        APITestFixture _fixtureLocal ;
+        RestClient _clientLocal;
+        public MediaIdGenerationRule(APITestFixture fixture)
+            : base("CARE", "CartoonFullAccessApiKey")
+        {
+            _airingObjectHelper = new AiringObjectHelper();
+            _fixtureLocal = fixture;
+            _clientLocal = this._fixtureLocal.restClient;            
+        }
+
+        [Fact, Order(1)]
+        public void PostAiring_WithVersion_MediaIdGenerationTest()
+        {
+            //JSON string with version with out AiringId, MediaID
+            string airingId = PostAiringTest(_airingObjectHelper.UpdateDates(Resources.Resources.CartoonAiringWith3Flights, 0), "Media Id Generation test");
+            
+            JObject response = new JObject();
+            var request = new RestRequest("/v1/airing/" + airingId, Method.GET);
+            Task.Run(async () =>
+            {
+                response = await _clientLocal.RetrieveRecord(request);
+
+            }).Wait();
+
+            // Assert
+            Assert.True(!string.IsNullOrEmpty(response.Value<string>(@"mediaId")), string.Format("Media Id {0} is generated", response.Value<string>(@"mediaId")));
+        }
+
+        [Fact, Order(1)]
+        public void PostAiring_WithOutVersion_MediaIdNonGenerationTest()
+        {
+            //JSON string with out version
+            string airingId = PostAiringTest(Resources.Resources.CartoonAiringWithNoVersion, "Media Id Non Generation test");
+
+            JObject Response = new JObject();
+            var Request = new RestRequest("/v1/airing/" + airingId, Method.GET);
+            Task.Run(async () =>
+            {
+                Response = await _clientLocal.RetrieveRecord(Request);
+
+            }).Wait();
+
+            // Assert
+            Assert.True(string.IsNullOrEmpty(Response.Value<string>(@"mediaId")), string.Format("Media Id is not generated"));
+        }
+
+    }
+
+}
