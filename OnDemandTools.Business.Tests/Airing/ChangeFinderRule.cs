565ec0eb83516d8981c8ee03e63d10eb1a55f5c4
Nirmala Subramaniam <nirmalasubu@yahoo.com>
2017-02-02 12:56:03 -0500

    ODTD-449 Changes without Mock

--- OnDemandTools.Business.Tests/Airing/ChangeFinderRule.cs
+++ OnDemandTools.Business.Tests/Airing/ChangeFinderRule.cs
@@ -2,15 +2,7 @@
 using Xunit;
 using Newtonsoft.Json.Linq;
 using OnDemandTools.Business.Modules.Airing;
-using Moq;
-using OnDemandTools.DAL.Modules.Airings;
-using OnDemandTools.DAL.Modules.Queue.Queries;
-using OnDemandTools.DAL.Modules.File.Queries;
-using OnDemandTools.DAL.Modules.Destination.Queries;
-using OnDemandTools.DAL.Modules.Package.Queries;
-using OnDemandTools.Common.Configuration;
-using System.Collections.Generic;
-using System;
+
 
 namespace OnDemandTools.Business.Tests.Airing
 {
@@ -18,77 +10,51 @@ namespace OnDemandTools.Business.Tests.Airing
     [Collection("Business Collection")]
     public class ChangeFinderRules
     {
-
-        private readonly Mock<IGetAiringQuery> _airingQueryHelper = new Mock<IGetAiringQuery>();
-        private readonly Mock<IAiringSaveCommand> _airingSaveCommandHelper = new Mock<IAiringSaveCommand>();
-        private readonly Mock<IAiringDeleteCommand> _airingDeleteCommandHelper = new Mock<IAiringDeleteCommand>();
-        private readonly Mock<IAiringMessagePusher> _airingMessagePusherCommandHelper = new Mock<IAiringMessagePusher>();
-        private readonly Mock<IQueueQuery> _queueQueryHelper = new Mock<IQueueQuery>();
-        private readonly Mock<ITaskUpdater> _taskUpdaterCommand = new Mock<ITaskUpdater>();
-        private readonly Mock<IFileQuery> _fileQueryHelper = new Mock<IFileQuery>();
-        private readonly Mock<IDestinationQuery> _destinationQueryHelper = new Mock<IDestinationQuery>();
-        private readonly Mock<IPackageQuery> _packageQueryHelper = new Mock<IPackageQuery>();
-        private readonly Mock<IChangeHistoricalAiringQuery> _changeHistoricalAiringQueryHelper = new Mock<IChangeHistoricalAiringQuery>();
-        private readonly Mock<IChangeDeletedAiringQuery> _changeDeletedAiringQueryHelper = new Mock<IChangeDeletedAiringQuery>();
-        private AppSettings appSettings = new AppSettings();
-        private readonly AiringService _airingService;
         BusinessTestFixture fixture;
 
-       public ChangeFinderRules(BusinessTestFixture fixture)
+        public ChangeFinderRules(BusinessTestFixture fixture)
         {
             this.fixture = fixture;
-
-            _airingService = new AiringService(
-            _airingQueryHelper.Object,
-            appSettings,
-            _airingSaveCommandHelper.Object,
-            _airingDeleteCommandHelper.Object, _airingMessagePusherCommandHelper.Object,
-            _queueQueryHelper.Object,
-            _taskUpdaterCommand.Object,
-            _fileQueryHelper.Object,
-           _destinationQueryHelper.Object,
-           _packageQueryHelper.Object,
-           _changeHistoricalAiringQueryHelper.Object,
-           _changeDeletedAiringQueryHelper.Object);
-        
-    } 
-      
+        }
 
         [Fact, Order(1)]
         public void GetAiringwithOptionChange_InitialReleaseTest()
         {
-            // Mocked the DAL layer object instead of getting from Mongo
-            List<DAL.Modules.Airings.Model.Airing> lstHistoryairings = new List<DAL.Modules.Airings.Model.Airing>();
-            lstHistoryairings.Add(JObject.Parse(Resources.AiringBusinessResource.ResourceManager.GetString("TBSEHistoryInitial")).ToObject<DAL.Modules.Airings.Model.Airing>());
-            _changeHistoricalAiringQueryHelper.Setup(cr => cr.Find(It.IsAny<List<string>>())).Returns(lstHistoryairings);
+            //Prepare
+            IAiringService airingService = fixture.container.GetInstance<IAiringService>();
+            var airingPost = JObject.Parse(Resources.AiringBusinessResource.ResourceManager.GetString("TBSEHistoryInitial")).ToObject<Modules.Airing.Model.Airing>();
+            var savedAiringObj = airingService.Save(airingPost, false, true);
 
-           // Call Business Method
-            var airing = JObject.Parse(Resources.AiringBusinessResource.ResourceManager.GetString("TBSEAiring")).ToObject<Modules.Airing.Model.Alternate.Long.Airing>();
-            _airingService.AppendChanges(ref airing);
+            JObject jsonObject = JObject.Parse(Resources.AiringBusinessResource.ResourceManager.GetString("TBSEAiring"));
+            jsonObject.Add("AiringId", savedAiringObj.AssetId);
+            var businessAiring = jsonObject.ToObject<Modules.Airing.Model.Alternate.Long.Airing>();
+
+            //Act
+            airingService.AppendChanges(ref businessAiring);
 
             //Assert
-           Assert.True(airing.Options.Changes[0].TheChange == "New Release", string.Format("The value returned for change should be 'New Release' but it is returned as {0}", airing.Options.Changes[0].TheChange));
+            Assert.True(businessAiring.Options.Changes.Count > 0, string.Format("The value returned for change should be 'New Release' but it is returned as {0}", businessAiring.Options.Changes[0].TheChange));
         }
 
         [Fact, Order(2)]
-        public void GetAiringwithOptionChange_ChangeTest()
+        public void GetAiringwithOptionChange_FlightEndDateModifiedTest()
         {
+            //Prepare
+            IAiringService airingService = fixture.container.GetInstance<IAiringService>();
+            var airingPost = JObject.Parse(Resources.AiringBusinessResource.ResourceManager.GetString("TBSEHistoryWithFightDateModified")).ToObject<Modules.Airing.Model.Airing>();
+            var savedAiringObj = airingService.Save(airingPost, false, true);
 
-            // Mocked the DAL layer object instead of getting from Mongo
-            List<DAL.Modules.Airings.Model.Airing> lstHistoryairings = new List<DAL.Modules.Airings.Model.Airing>();
-            lstHistoryairings.Add(JObject.Parse(Resources.AiringBusinessResource.ResourceManager.GetString("TBSEHistoryInitial")).ToObject<DAL.Modules.Airings.Model.Airing>());
-            lstHistoryairings.Add(JObject.Parse(Resources.AiringBusinessResource.ResourceManager.GetString("TBSEHistoryWithFightDateModified")).ToObject<DAL.Modules.Airings.Model.Airing>());
-            _changeHistoricalAiringQueryHelper.Setup(cr => cr.Find(It.IsAny<List<string>>())).Returns(lstHistoryairings);
+            JObject jsonObject = JObject.Parse(Resources.AiringBusinessResource.ResourceManager.GetString("TBSEAiring"));
+            jsonObject.Add("AiringId", savedAiringObj.AssetId);
+            var businessAiring = jsonObject.ToObject<Modules.Airing.Model.Alternate.Long.Airing>();
 
-            // Arrange
-            var airing = JObject.Parse(Resources.AiringBusinessResource.ResourceManager.GetString("TBSEAiring")).ToObject<Modules.Airing.Model.Alternate.Long.Airing>();
-            _airingService.AppendChanges(ref airing);
+            //Act
+            airingService.AppendChanges(ref businessAiring);
 
-            //Assert
-            Assert.True(airing.Options.Changes[0].TheChange == "Flights's Start", string.Format("The value returned for change should be 'Flights's Start' but it is returned as {0}", airing.Options.Changes[0].TheChange));
-            Assert.True(airing.Options.Changes[1].TheChange == "Flights's End", string.Format("The value returned for change should be 'Flights's End' but it is returned as {0}", airing.Options.Changes[1].TheChange));
+            //Assert           
+            Assert.True(businessAiring.Options.Changes[1].TheChange == "Flights's End", string.Format("The value returned for change should be 'Flights's End' but it is returned as {0}", businessAiring.Options.Changes[1].TheChange));
         }
 
-       
+
     }
 }
