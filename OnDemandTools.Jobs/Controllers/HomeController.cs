dbe171f9b0c6781268c28426d759ab2af6d64f95
Nirmala Subramaniam <nirmalasubu@yahoo.com>
2017-02-09 10:56:19 -0500

    ODTD-465  Job route Health Check

--- OnDemandTools.Jobs/Controllers/HomeController.cs
+++ OnDemandTools.Jobs/Controllers/HomeController.cs
@@ -11,6 +11,9 @@ using OnDemandTools.Jobs.JobRegistry.Publisher;
 using OnDemandTools.Jobs.JobRegistry.Deporter;
 using OnDemandTools.Business.Modules.Queue;
 using OnDemandTools.Common.Configuration;
+using Hangfire.Storage;
+using OnDemandTools.Jobs.Model;
+using RestSharp;
 using FBDService;
 using OrionService;
 
@@ -43,6 +46,7 @@ namespace OnDemandTools.Jobs.Controllers
             return Redirect("/dashboard");
         }
 
+        [Route("/Whoami")]
         public IActionResult Whoami()
         {
             //TODO - add code similar to the API whoami
@@ -53,8 +57,36 @@ namespace OnDemandTools.Jobs.Controllers
         [Route("/healthcheck")]
         public JsonResult Healthcheck()
         {
+
+            Healthcheck hCheck = new Healthcheck();          
+            var con =JobStorage.Current.GetConnection();
+            var servers = JobStorage.Current.GetMonitoringApi().Servers();
+            var server = JobStorage.Current.GetComponents();
+            var dateTimeExpire = DateTime.UtcNow.AddMinutes(-1);
+            if (servers.All(x => x.Heartbeat.HasValue && x.Heartbeat < dateTimeExpire))
+            {
+                hCheck.IsAppHealthy = false;  //server stop alert
+            }
+            var DeporterJob = con.GetAllEntriesFromHash($"recurring-job:{"Deporter"}");
+            hCheck.DeporterAgentsHealth = GetJobState(con, DeporterJob)== "Succeeded" ? "excellent": "critical";
+
+            List<string> QueueJobsStatus = new List<string>();
+            var activeQueues=queueService.GetByStatus(true);
+            foreach (var activeQueue in activeQueues)
+            {
+                var QueueJob = con.GetAllEntriesFromHash($"recurring-job:{string.Format("Publisher-{0}", activeQueue.Name)}"); 
+                QueueJobsStatus.Add(GetJobState(con, QueueJob));
+            }
+            int succeedQueueJob = QueueJobsStatus.Where(x => x == "Succeeded").Count();
+            double postOfficePCT = ((Double)succeedQueueJob / activeQueues.Count()) * 100;
+            if (postOfficePCT == 100.0) hCheck.PublisherAgentsHealth = "excellent";
+            if (postOfficePCT < 100.0 && postOfficePCT >= 50.0) hCheck.PublisherAgentsHealth = "moderate";
+            if (postOfficePCT < 50.0) hCheck.PublisherAgentsHealth = "critical";
            
-            return Json("Healthy");
+            var TitleSyncJob = con.GetAllEntriesFromHash($"recurring-job:{"TitleSync"}");
+            hCheck.TitleSyncAgentsHealth = GetJobState(con, TitleSyncJob) == "Succeeded" ? "excellent" : "critical";
+
+            return Json(hCheck);
         }
 
         public IActionResult Heartbeat()
@@ -136,5 +168,39 @@ namespace OnDemandTools.Jobs.Controllers
                 throw e;
             }
         }
+
+
+        #region Private methods
+
+        private string GetJobState(IStorageConnection con, Dictionary<string, string> Job)
+        {
+            string state = "Failed";
+            if (Job != null && Job.ContainsKey("LastJobId"))
+            {
+                var d = con.GetJobData(Job["LastJobId"]);
+                state = d.State;
+            }
+            return state;
+        }
+
+
+        private Task<string> GetHostingProviderDetails(RestClient theClient, RestRequest theRequest)
+        {
+            var tcs = new TaskCompletionSource<String>();
+            theClient.ExecuteAsync(theRequest, response =>
+            {
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    tcs.SetResult(response.Content);
+                }
+                else
+                {
+                    tcs.SetResult(string.Empty);
+                }
+
+            });
+            return tcs.Task;
+        }
+        #endregion
     }
 }
