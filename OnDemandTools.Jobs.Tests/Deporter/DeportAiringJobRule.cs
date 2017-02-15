675b69e8585e7a3139ade7ad03bea085e4b95099
Nirmala Subramaniam <nirmalasubu@yahoo.com>
2017-02-15 14:22:12 -0500

    ODTD-474  Deporter Job unit Test added

---
+++ OnDemandTools.Jobs.Tests/Deporter/DeportAiringJobRule.cs
@@ -0,0 +1,73 @@
+﻿using Newtonsoft.Json.Linq;
+using OnDemandTools.Business.Modules.Airing;
+using OnDemandTools.Jobs.Tests.Helpers;
+using RestSharp;
+using System;
+using BLModel = OnDemandTools.Business.Modules.Airing.Model;
+using System.Threading.Tasks;
+using Xunit;
+using Microsoft.Extensions.Configuration;
+using System.Threading;
+
+namespace OnDemandTools.Jobs.Tests.Deporter
+{
+    [TestCaseOrderer("OnDemandTools.Jobs.Tests.Helpers.CustomTestCaseOrderer", "OnDemandTools.Jobs.Tests")]
+    [Collection("Job Collection")]
+    public class DeportAiringJobRule
+    {
+        JobTestFixture fixture;
+        RestClient _client;
+       
+        public DeportAiringJobRule(JobTestFixture fixture)
+        {
+            this.fixture = fixture;
+            _client = this.fixture.restClient;
+        }
+
+        [Fact, Order(1)]
+        public void VerifyDeporterJobTest()
+        {
+            //Prepare
+            IAiringService airingService = fixture.container.GetInstance<IAiringService>();
+            JObject airingJson = JObject.Parse(Resources.Resource.ResourceManager.GetString("AiringWithSingleFlight"));
+            JObject response = new JObject();
+            var request = new RestRequest("/v1/airing/TBSE", Method.POST);
+            request.AddParameter("application/json", UpdateAiringDates(airingJson), ParameterType.RequestBody);
+
+            Task.Run(async () =>
+            {
+                response = await _client.RetrieveRecord(request);
+
+            }).Wait();
+            
+            string airingId = response.Value<string>(@"airingId");
+
+            //Act 
+            airingService.Deport(int.Parse(fixture.Configuration["airingDeportGraceDays"]));
+
+            //Assert
+            BLModel.Airing expiredairingModel= airingService.GetBy(airingId, AiringCollection.ExpiredCollection);
+
+            if (expiredairingModel == null)
+            {
+                Assert.True(false, "Deporter Airing test Failed : Airirng is  not returned from Expired Collection : " + expiredairingModel.AssetId);
+            }
+        }
+        
+        private JObject UpdateAiringDates(JObject jObject)
+        {
+           
+            JArray jArray = (JArray)jObject.SelectToken("Flights");
+
+            foreach (JObject obj in jArray)
+            {
+                obj["Start"] = DateTime.UtcNow.AddDays(-2);
+                obj["End"] = DateTime.UtcNow.AddDays(-1);                
+            }
+
+            jObject["ReleasedOn"] = DateTime.UtcNow.AddDays(-int.Parse(fixture.Configuration["airingDeportGraceDays"]));
+            return jObject;
+
+        }
+    }
+}
