0c953aa33d8be56187b680287c7254f394e4d5c7
c-hclent-ssoundarara <Sureshkumar.Soundararajan@turner.com>
2017-03-31 04:39:56 -0400

    ODT-552 properties values test2

--- OnDemandTools.API.Tests/AiringRoute/GetAiringByDestinationRule.cs
+++ OnDemandTools.API.Tests/AiringRoute/GetAiringByDestinationRule.cs
@@ -44,7 +44,7 @@ namespace OnDemandTools.API.Tests.AiringRoute
         public void GetAiring_WithValidDestinationTest()
         {
             JArray response = default(JArray);
-            var request = new RestRequest("/v1/airings/destination/CNWB", Method.GET);
+            var request = new RestRequest("/v1/airings/destination/DISHB", Method.GET);
             Task.Run(async () =>
             {
                 response = await client.RetrieveRecords(request);
@@ -54,14 +54,24 @@ namespace OnDemandTools.API.Tests.AiringRoute
             if (response.Count > 0)
             {
                 var my_obj = response.First.Value<JArray>(@"flights").First.Value<JArray>(@"destinations").ToList();
-                var message = my_obj.Any(i => i.Value<String>(@"name") == "CNWB") ? "Test Passed" : "User do not have access to destination";
-                Assert.True(my_obj.Any(i => i.Value<String>(@"name") == "CNWB"), message);
+                var message = my_obj.Any(i => i.Value<String>(@"name") == "DISHB") ? "Test Passed" : "User do not have access to destination";
+                Assert.True(my_obj.Any(i => i.Value<String>(@"name") == "DISHB"), message);
 
-                var flightsDestinationToken = response.First[@"flights"].First["destinations"];
-                var propertiesToken = flightsDestinationToken.First["properties"];
-                var deliverablesToken = flightsDestinationToken.First["deliverables"];
-                Assert.Null(propertiesToken.First);
-                Assert.Null(deliverablesToken.First);
+                foreach (var jobj in response)
+                {
+                    var flightsDestinationToken = jobj[@"flights"].First["destinations"];
+                    var propertiesToken = flightsDestinationToken.First["properties"];
+                    if (propertiesToken.HasValues)
+                    {
+                        Assert.True((flightsDestinationToken.First["name"].ToString() == "DISHB"),
+                            string.Format("Destination should be " + flightsDestinationToken.First["name"].ToString() + "and but the returned {0}", flightsDestinationToken.First["name"].ToString()));
+                        Assert.True((propertiesToken.First["name"].ToString() == "Description"),
+                            string.Format("Property name should be " + propertiesToken.First["name"].ToString() + "and but the returned {0}", propertiesToken.First["name"].ToString()));
+                        Assert.True((propertiesToken.First["value"].ToString() == "Dish for Broadband"),
+                            string.Format("Property value should be " + propertiesToken.First["value"].ToString() + "and but the returned {0}", propertiesToken.First["value"].ToString()));
+                        break;
+                    }
+                }
             }
         }
     }
