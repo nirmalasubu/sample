f877d1c0d35ff86b8b3e6965773a6d0e65da05d4
c-hclent-ssoundarara <Sureshkumar.Soundararajan@turner.com>
2017-04-21 06:30:37 -0400

    ODT-572 UI styling and partitioning the page into components

--- OnDemandTools.Web/App/Components/App.jsx
+++ OnDemandTools.Web/App/Components/App.jsx
@@ -1,29 +1,9 @@
 import React  from 'react';
-import {Link} from 'react-router';
+import Layout from './Common/PageLayout';
 
 const App = (props) => {
-  return (
-    <div className="container">
-      <nav className="navbar navbar-default">
-        <div className="container-fluid">
-          <div className="navbar-header">
-            <a className="navbar-brand" href="#">OnDemand Tools</a>
-          </div>
-          <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul className="nav navbar-nav">
-              <li><Link to="/">Home</Link></li>
-              <li><Link to="/destinations">Destinations</Link></li>
-              <li><Link to="/products">Products</Link></li>
-              <li><Link to="/deliveryQueues">Delivery Queues</Link></li>
-              <li><Link to="/pendingRequests">Pending Requests</Link></li>
-              <li><Link to="/permissions">Permissions</Link></li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-      {/* Each smaller components */}
-      {props.children}
-    </div>
+    return (
+      <Layout content = {props}/>
   );
 };
 
