539586fed0c07ae76a5bf7355bcbd76b35ee7585
Shanmuga Sivagnanam <shanmuga.sundaram@turner.com>
2017-04-24 11:56:12 -0400

    ODT-572 Header style changes

--- OnDemandTools.Web/App/Components/Common/Header.jsx
+++ OnDemandTools.Web/App/Components/Common/Header.jsx
@@ -1,15 +1,19 @@
 ﻿import React from 'react';
-import {PageHeader} from 'react-bootstrap';
-import {Image} from 'react-bootstrap';
+import { PageHeader } from 'react-bootstrap';
+import { Image } from 'react-bootstrap';
 
 const Header = () => {
-    return (       
-        <div >            
-            <PageHeader ><Image src="../images/ODTLogo.png" rounded /><small style={alignLeft}>Welcome Cea ! <a href="/account/logoff" >Logout</a></small></PageHeader>  
+    return (
+        <div >
+            <Image src="../images/ODTLogo.png" rounded />
+            <div style={alignLeft} >
+               <h4> Welcome Cea ! <a href="/account/logoff" >Logout</a></h4>
+            </div>
+            <hr/>
         </div>
-  );
+    );
 };
 
-const alignLeft = {float : 'right'};
+const alignLeft = { float: 'right' };
 
 export default Header
