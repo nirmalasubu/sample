3fd68a81f8619ea6528b158135c3449c81c94dad
Shanmuga Sivagnanam <shanmuga.sundaram@turner.com>
2017-04-26 14:28:19 -0400

    ODT-570 added missing actions

--- OnDemandTools.Web/App/Actions/ActionTypes.jsx
+++ OnDemandTools.Web/App/Actions/ActionTypes.jsx
@@ -1,2 +1,3 @@
 ﻿//placeholder to provider all the Action Reducer Name
 export const FETCH_USERDETAILS_SUCCESS = 'FETCH_USERDETAILS_SUCCESS';
+export const FETCH_QUEUES_SUCCESS = 'FETCH_QUEUES_SUCCESS';
