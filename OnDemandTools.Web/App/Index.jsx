09b4bcca5f561e9c737522ee7ce56bd9844a2915
Shanmuga Sivagnanam <shanmuga.sundaram@turner.com>
2017-04-26 13:54:03 -0400

    ODT-570 Unwanted changes removed

--- OnDemandTools.Web/App/Index.jsx
+++ OnDemandTools.Web/App/Index.jsx
@@ -4,14 +4,12 @@ import { Provider } from 'react-redux';
 import { render } from 'react-dom';
 import { Router, browserHistory } from 'react-router';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-// import '../node_modules/font-awesome/cs/s/font-awesome.css';
 import routes from 'Routes';
 import configureStore from 'Store/ConfigureStore';
 import '../wwwroot/css/site.css';
 require('font-awesome/css/font-awesome.css');
 
 const store = configureStore();
-//store.dispatch(userActions.fetchUser());
 
 render(
     <Provider store={store}>
