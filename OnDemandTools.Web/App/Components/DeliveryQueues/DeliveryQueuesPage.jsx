fc7fe978f04ca06ed8e04eee1254a9e263f93555
Nirmala Subramaniam <nirmalasubu@yahoo.com>
2017-04-28 17:25:09 -0400

    ODT-690 react  boot starp table

--- OnDemandTools.Web/App/Components/DeliveryQueues/DeliveryQueuesPage.jsx
+++ OnDemandTools.Web/App/Components/DeliveryQueues/DeliveryQueuesPage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import * as queueActions from 'Actions/DeliveryQueue/DeliveryQueueActions';
 import DeliveryQueueFilter from 'Components/DeliveryQueues/DeliveryQueueFilter';
+import DeliveryQueueTable from 'Components/DeliveryQueues/table';
 import $ from 'jquery';
 
 
@@ -12,153 +13,80 @@ class Queue extends React.Component {
         // this.sortColumn = this.sortColumn.bind(this);
         this.state = {
             stateQueue: [],
-            sortClass: [{ "friendlyName": "fa fa-sort" },
-            { "hoursOut": "fa fa-sort" },
-            { "contactEmailAddress": "fa fa-sort" }],
+
             filterValue : {
                 queueName : "",
                 contactName : "",
                 queueId : ""
-            }
-        }
+            },
+            
+            columns:[{"label":"Queue Name","dataField":"friendlyName","sort":true},
+                    {"label":"Advanced Delivery","dataField":"hoursOut","sort":true},
+                    {"label":"Contact","dataField":"contactEmailAddress","sort":true},
+                    {"label":"Remote Queue","dataField":"name","sort":false},
+                    {"label":"Actions","dataField":"","sort":false}
+            ],
+            RowData:[]
+
+    }
     }
 
     handleFilterUpdate(filtersValue, type) {
-            var valuess = this.state.filterValue;
-            if(type=="QN")
-                valuess.queueName = filtersValue;
+        var valuess = this.state.filterValue;
+        if(type=="QN")
+            valuess.queueName = filtersValue;
 
-            if(type=="CN")
-                valuess.contactName = filtersValue;
+        if(type=="CN")
+            valuess.contactName = filtersValue;
 
-            if(type=="QI")
-                valuess.queueId = filtersValue;
+        if(type=="QI")
+            valuess.queueId = filtersValue;
 
-            if(type=="CL")
-            {
-                this.state.filterValue.queueName="";
-                this.state.filterValue.contactName="";
-                this.state.filterValue.queueId="";
-            }
+        if(type=="CL")
+        {
+            this.state.filterValue.queueName="";
+            this.state.filterValue.contactName="";
+            this.state.filterValue.queueId="";
+        }
 
+        this.setState({
+            filterValue: valuess
+        });
+
+        if(this.state.filterValue.queueName!="" || this.state.filterValue.contactName!="" || this.state.filterValue.queueId!="")
+        {
+            var friendlyName = this.state.filterValue.queueName.toLowerCase();
+            var contactName = this.state.filterValue.contactName.toLowerCase();
+            var queueId = this.state.filterValue.queueId.toLowerCase();
+
+            var queueArray = $.grep(this.props.queues, function(v) {
+                return ((friendlyName !=""?v.friendlyName.toLowerCase().indexOf(friendlyName) != -1:true)
+                    && (contactName!=""?v.contactEmailAddress.toLowerCase().indexOf(contactName) != -1:true)
+                    && (queueId!=""?v.name.toLowerCase().indexOf(queueId) != -1:true));
+            });
             this.setState({
-                filterValue: valuess
+                stateQueue: queueArray
+            });
+        }
+        else
+            this.setState({
+                stateQueue: this.props.queues
             });
 
-            if(this.state.filterValue.queueName!="" || this.state.filterValue.contactName!="" || this.state.filterValue.queueId!="")
-            {
-                var friendlyName = this.state.filterValue.queueName.toLowerCase();
-                var contactName = this.state.filterValue.contactName.toLowerCase();
-                var queueId = this.state.filterValue.queueId.toLowerCase();
-                
-                var queueArray = $.grep(this.props.queues, function(v) {
-                    return ((friendlyName !=""?v.friendlyName.toLowerCase().indexOf(friendlyName) != -1:true) 
-                        && (contactName!=""?v.contactEmailAddress.toLowerCase().indexOf(contactName) != -1:true)
-                        && (queueId!=""?v.name.toLowerCase().indexOf(queueId) != -1:true));                    
-                });
-                this.setState({
-                    stateQueue: queueArray
-                });
-            }
-            else
-                this.setState({
-                    stateQueue: this.props.queues
-                });
-        
     }
 
     //called on the page load
     componentDidMount() {
+       
         let promise = this.props.fetchQueue();
         promise.then(newqueue => {
             this.setState({
-                stateQueue: this.props.queues
+                stateQueue: this.props.queues,
+                   
             })
         });
+                   
         document.title = "ODT - Delivery Queues";
-    }
-
-    sortString(Name) {
-        var queueArray = [];
-        var sortDesc = false;
-        this.state.sortClass.map(function (css) {
-            for (var key in css) {
-                if(key==Name)
-                {
-                    if(css[key]=="fa fa-sort-asc" )
-                    { 
-                        css[key]="fa fa-sort-desc"
-                        sortDesc=true;
-                    }
-                    else {
-                        css[key] = "fa fa-sort-asc"
-                    }
-                } else {
-                    css[key] = "fa fa-sort"
-                }
-
-            }
-        })
-
-        if (sortDesc) {
-            queueArray = this.state.stateQueue.sort((a, b) => {
-                var nameA = a[Name].toLowerCase(), nameB = b[Name].toLowerCase()
-                if (nameA > nameB) //sort string descending
-                    return -1
-                if (nameA < nameB)
-                    return 1
-                return 0
-            })
-        }
-        else {
-            queueArray = this.state.stateQueue.sort((a, b) => {
-                var nameA = a[Name].toLowerCase(), nameB = b[Name].toLowerCase()
-                if (nameA < nameB) //sort string ascending
-                    return -1
-                if (nameA > nameB)
-                    return 1
-                return 0
-            })
-        }
-        this.setState({
-            stateQueue: queueArray
-        })
-    }
-
-
-    sortNumber(Value) {
-        var queueArray = [];
-        var sortDesc = false;
-        this.state.sortClass.map(function (css) {
-            for (var key in css) {
-                if (key == Value) {
-                    if (css[key] == "fa fa-sort-asc") {
-                        css[key] = "fa fa-sort-desc"
-                        sortDesc = true;
-                    }
-                    else {
-                        css[key] = "fa fa-sort-asc"
-                    }
-                } else {
-                    css[key] = "fa fa-sort"
-                }
-
-            }
-        })
-
-        if (sortDesc) {
-            queueArray = this.state.stateQueue.sort((a, b) => {
-                return a[Value] - b[Value]
-            })
-        }
-        else {
-            queueArray = this.state.stateQueue.sort((a, b) => {
-                return b[Value] - a[Value]
-            })
-        }
-        this.setState({
-            stateQueue: queueArray
-        })
     }
 
     render() {
@@ -167,55 +95,7 @@ class Queue extends React.Component {
                   <label className="queue-head">Delivery Queues</label>
                   <hr/>
                 <DeliveryQueueFilter updateFilter={this.handleFilterUpdate.bind(this)} />
-
-                <div className="row">                   
-                    <div className="col-xs-12">
-                        <table id="queueTable" className="table table-hover table-striped table-bordered table-responsive">
-                            <thead>
-                                <tr>
-                                    <th onClick={this.sortString.bind(this, 'friendlyName')}>Queue Name <i class={this.state.sortClass[0].friendlyName} /></th>
-                                    <th onClick={this.sortNumber.bind(this, 'hoursOut')} className="queue-table-th-advanceddevlivery" >Advanced Delivery  <i class={this.state.sortClass[1].hoursOut} /></th>
-                                    <th onClick={this.sortString.bind(this, 'contactEmailAddress')}>Contact  <i class={this.state.sortClass[2].contactEmailAddress} /></th>
-                                    <th  >Remote Queue</th>
-                                    <th >Actions</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {this.state.stateQueue.map((item, index) => {
-                                    return (
-                                        <tr key={index}>
-                                            <td  >{item.friendlyName}</td>
-                                            <td className="queue-table-th-advanceddevlivery">{item.hoursOut}</td>
-                                            <td ><p data-toggle="tooltip" title={item.contactEmailAddress}>{item.contactEmailAddress}</p></td>
-
-                                            <td  >
-                                                <p>
-                                                    {item.name}
-                                                    <br />
-                                                    <br />
-                                                    <i>Delivery</i>: 0
-                        <button class="btn-xs btn-link" title="clear pending deliveries to queue">
-                                                        Clear
-                        </button>
-                                                    <button class="btn-xs btn-link" title="clear pending deliveries to queue">
-                                                        Resend
-                           </button>
-                                                    <br />
-                                                    <i>Consumption</i>: 21
-                        <button class="btn-xs btn-link">
-                                                        Purge
-                        </button>
-                                                    <span class="small">-Apr 25, 2017 2:24 PM</span>
-                                                </p>
-                                            </td>
-                                            <td><i class="fa fa-search" aria-hidden="true"></i> <i class="fa fa-calendar" aria-hidden="true"></i></td>
-                                        </tr>
-                                    );
-                                })}
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
+                <DeliveryQueueTable  RowData={this.state.stateQueue} ColumnData={this.state.columns}/>
             </div>
 
         )
@@ -242,3 +122,5 @@ const mapDispatchToProps = (dispatch) => {
 
 // Use connect to put them together
 export default connect(mapStateToProps, mapDispatchToProps)(Queue);
+
+
