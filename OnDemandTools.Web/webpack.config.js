ab0c662adcdb59f0b3dfc89cfb45d354eb953890
jjohn <dormantroot@gmail.com>
2017-05-01 13:18:38 -0400

    added bundle minification

--- OnDemandTools.Web/webpack.config.js
+++ OnDemandTools.Web/webpack.config.js
@@ -63,7 +63,19 @@ module.exports = {
 
     },
     plugins: [
-        new CaseSensitivePathsPlugin()
+        new CaseSensitivePathsPlugin(),
+        new webpack.DefinePlugin({ 
+            'process.env': {
+                'NODE_ENV': JSON.stringify('production')
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            minimize: true,
+            compress: {
+                warnings: false
+            }
+        }),
+        new webpack.optimize.AggressiveMergingPlugin()
         
     ],
 };
