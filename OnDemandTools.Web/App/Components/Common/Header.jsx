f62bfc1c392cf03bb1eab0652c0c275822dd71c5
Shanmuga Sivagnanam <shanmuga.sundaram@turner.com>
2017-05-02 13:31:18 -0400

    Connect implemented for User action

--- OnDemandTools.Web/App/Components/Common/Header.jsx
+++ OnDemandTools.Web/App/Components/Common/Header.jsx
@@ -2,58 +2,37 @@
 import { PageHeader } from 'react-bootstrap';
 import { Image } from 'react-bootstrap';
 import { connect } from 'react-redux';
-import * as userActions from 'Actions/User/UserActions';
+import { fetchUser } from 'Actions/User/UserActions';
 import $ from 'jquery';
 
-class Header extends React.Component{
-    constructor(props){
+@connect((store) => {
+    return {
+        user: store.user,
+    };
+})
+class Header extends React.Component {
+    constructor(props) {
         super(props);
-
-        this.state = {
-            name:"Guest"
-        } 
     }
-    
-    //called on the page load
-    componentDidMount() {       
-        let promise =  this.props.fetchUser();
-        promise.then(newuser => {
-            this.setState({
-                name: this.props.user
-            })
-        });
-      
-    }    
 
-    render(){
+    //called on the page load
+    componentDidMount() {
+        this.props.dispatch(fetchUser())
+    }
+
+    render() {
         return (
-        <div >
+            <div >
                 <Image src="../images/ODTLogo.png" rounded />
                 <div style={alignLeft} >
-                   <h4> Welcome { this.state.name }! <a href="/account/logoff" >Logout</a>&nbsp;&nbsp;&nbsp;</h4>
+                    <h4> Welcome {this.props.user}! <a href="/account/logoff" >Logout</a>&nbsp;&nbsp;&nbsp;</h4>
                 </div>
-            <hr/>
-        </div>
-    )
+                <hr />
+            </div>
+        )
     }
 }
 
 const alignLeft = { float: 'right' };
 
-// Maps state from store to props
-const mapStateToProps = (state, ownProps) => {   
-    return {
-        // You can now say this.props.user        
-        user: state.user
-    }
-};
-
-// Maps actions to props
-const mapDispatchToProps = (dispatch) => {    
-    return {
-        fetchUser: () => dispatch(userActions.fetchUser())
-    };
-};
-
-// Use connect to put them together
-export default connect( mapStateToProps,mapDispatchToProps)(Header);
+export default Header;
