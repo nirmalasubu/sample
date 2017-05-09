328f7682e1f9a61183b5c30472b4c2cb9a8d34ae
Shanmuga Sivagnanam <shanmuga.sundaram@turner.com>
2017-05-09 13:25:12 -0400

    ODT-582 Controls added

--- OnDemandTools.Web/App/Components/DeliveryQueues/QueueResetByDateRange.jsx
+++ OnDemandTools.Web/App/Components/DeliveryQueues/QueueResetByDateRange.jsx
@@ -6,8 +6,9 @@ import { ModalBody } from 'react-bootstrap';
 import { ModalFooter } from 'react-bootstrap';
 import { ModalHeader } from 'react-bootstrap';
 import { ModalTitle } from 'react-bootstrap';
-import { Button } from 'react-bootstrap';
 import * as queueAction from 'Actions/DeliveryQueue/DeliveryQueueActions';
+import { Grid, Row, Col, InputGroup, Radio, Form, ControlLabel, FormGroup, FormControl, Button } from 'react-bootstrap';
+import DatePicker from "react-bootstrap-date-picker";
 
 var QueueResetByDateRange = React.createClass({
     resetQueue(id){ 
@@ -16,24 +17,58 @@ var QueueResetByDateRange = React.createClass({
     },
     render: function() {
         return (
-           <Modal show={this.props.data.showDateRangeResetModel} onHide={this.props.handleClose}>
+           <Modal bsSize="large" show={this.props.data.showDateRangeResetModel} onHide={this.props.handleClose}>
                  <Modal.Header closeButton>
                     <Modal.Title>
                         <p>Delivery by Date Range to {this.props.data.queueDetails.friendlyName}</p>
                     </Modal.Title>
                 </Modal.Header>         
                 <Modal.Body>
-                    {
-                        <p>Reset queue by date range for Queue {this.props.data.queueDetails.friendlyName}</p>
-                    }
+                    <ControlLabel>This query does not permanently override the query in the Queue nor does it persists once this window is closed and the delivery is made. </ControlLabel>
+                    <Form inline>
+                        <ControlLabel>Delivery Airings where: </ControlLabel>
+                        <FormGroup>
+                            <InputGroup>
+                                <ControlLabel> &nbsp;&nbsp;</ControlLabel>
+                                <Radio name="dateAvailibilityWindow">
+                                    &nbsp; Start date falls in availability window
+                                </Radio>
+                                <ControlLabel> &nbsp;&nbsp;</ControlLabel>
+                                <Radio name="dateAvailibilityWindow">
+                                    &nbsp; Any Part of flight falls in availability window
+                                 </Radio>
+                            </InputGroup>
+                        </FormGroup>
+                    </Form>
+                    <Form inline>
+                        <ControlLabel>Starting Between: </ControlLabel>{'  '}
+                        <FormGroup >
+                            <DatePicker style={datePickerWidth} showClearButton={false} />
+                        </FormGroup>
+                        {'  AND  '}
+                        <FormGroup>
+                            <DatePicker style={datePickerWidth} showClearButton={false} />
+                        </FormGroup>
+                    </Form>
+
+                    <Form inline>
+                        <FormGroup controlId="queryInput">                            
+                            <FormControl componentClass="textarea" placeholder="Query" />
+                        </FormGroup>
+                        <FormGroup controlId="queryResults">                           
+                            <FormControl componentClass="textarea" placeholder="Results" />
+                        </FormGroup>
+                    </Form>
                 </Modal.Body>
                 <Modal.Footer>
                     <Button onClick={this.props.handleClose}>Cancel</Button>  
-                    <Button onClick={this.resetQueue.bind(this, this.props.data.queueDetails.name)}>Continue</Button>
+                    <Button onClick={this.resetQueue.bind(this, this.props.data.queueDetails.name)}>Run Query</Button>
                 </Modal.Footer>
            </Modal>
         )
     }
 });
+
+const datePickerWidth = { width: '400px' };
 
 export default QueueResetByDateRange;
