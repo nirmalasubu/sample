33d1eeab00e13e1d7eb802e91b880650ae1b243d
Nirmala Subramaniam <nirmalasubu@yahoo.com>
2017-05-12 16:07:24 -0400

    changes DeliveryQueue Fliter to ES6

--- OnDemandTools.Web/App/Components/DeliveryQueues/DeliveryQueueFilter.jsx
+++ OnDemandTools.Web/App/Components/DeliveryQueues/DeliveryQueueFilter.jsx
@@ -2,8 +2,14 @@ import React from 'react';
 import { Form, ControlLabel, FormGroup, FormControl, Button } from 'react-bootstrap';
 import $ from 'jquery';
 
-var DeliveryQueueFilter = React.createClass({
-    handleChange: function() {
+class DeliveryQueueFilter extends React.Component
+{
+    
+    constructor(props) {
+        super(props);
+      
+    }
+    handleChange() {
         var qn = this.inputQueueName.value;
         this.props.updateFilter(qn, "QN");
 
@@ -12,41 +18,41 @@ var DeliveryQueueFilter = React.createClass({
 
         var qi = this.inputQueueId.value;
         this.props.updateFilter(qi, "QI");
-    },
+    }
 
-    clearFilter : function(){
+    clearFilter (){
         this.inputQueueName.value = "";
         this.inputContactName.value = "";
         this.inputQueueId.value = "";
 
         this.props.updateFilter("", "CL");
-    },
+    }
 
-    render: function() {
+    render() {
         return (
             <div>
                 <Form inline>
                     <ControlLabel>Filter by: </ControlLabel>
         {' '}
         <FormGroup controlId="queueName">
-            <FormControl type="text" ref="filterInput" inputRef = {(input) => this.inputQueueName = input } onChange={this.handleChange} placeholder="Queue Name" />
-        </FormGroup>
+                <FormControl type="text" inputRef = {(input) => this.inputQueueName = input } onChange={this.handleChange.bind(this) }  placeholder="Queue Name" />
+            </FormGroup>
         {' '}
         <FormGroup controlId="contactName">
-            <FormControl type="text" inputRef = {(input) => this.inputContactName = input } onChange={this.handleChange}  placeholder="Contact Name" />
+            <FormControl type="text" inputRef = {(input) => this.inputContactName = input } onChange={this.handleChange.bind(this) }  placeholder="Contact Name" />
         </FormGroup>
         {' '}
         <FormGroup controlId="queueId">
-            <FormControl type="text" inputRef = {(input) => this.inputQueueId = input } onChange={this.handleChange} placeholder="Queue ID" />
-        </FormGroup>
+                   <FormControl type="text" inputRef = {(input) => this.inputQueueId = input } onChange={this.handleChange.bind(this) }  placeholder="Queue Id" />
+               </FormGroup>
         {' '}
-        <Button onClick={this.clearFilter} bsStyle="primary">
+        <Button onClick={this.clearFilter.bind(this)} bsStyle="primary">
             Clear All Filters
         </Button>
     </Form>
 </div>
-        )
+       )
     }
-});
+}
 
 export default DeliveryQueueFilter;
