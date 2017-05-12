64710636455bb766c6e1c2b8d9548376d4f19830
c-hclent-ssoundarara <Sureshkumar.Soundararajan@turner.com>
2017-05-12 03:20:52 -0400

    ODT-582 new files

---
+++ OnDemandTools.Business/Modules/Queue/Model/DeliverCriteria.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnDemandTools.Business.Modules.Queue.Model
+{
+    public class DeliverCriteria
+    {
+        public string Name { get; set; }
+        public string Query { get; set; }
+
+        public int WindowOption { get; set; }
+
+        public DateTime StartDateTime { get; set; }
+        public DateTime EndDateTime { get; set; }
+    }
+}
---
+++ OnDemandTools.Web/Models/DeliveryQueue/DeliveryCriteriaModel.cs
@@ -0,0 +1,19 @@
+﻿using OnDemandTools.Common.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnDemandTools.Web.Models.DeliveryQueue
+{
+    public class DeliverCriteriaModel
+    {
+        public string Name { get; set; }
+        public string Query { get; set; }
+
+        public int WindowOption { get; set; }
+
+        public DateTime StartDateTime { get; set; }
+        public DateTime EndDateTime { get; set; }
+    }
+}
