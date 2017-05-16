24933b5fa0f58267c3db0a20ffdca4f983189e75
joseph <joseph@josephbulger.com>
2017-05-16 11:43:04 -0400

    cleaning up the error statuses

--- OnDemandTools.API.Tests/apitestrun.sh
+++ OnDemandTools.API.Tests/apitestrun.sh
@@ -2,4 +2,8 @@
 
 dotnet test -xml /app/output/apirun.xml
 
+BOMBED=$?
+
 xsltproc --output /app/output/apiresults.xml --stringparam use.extensions 0 /app/xunit-to-junit.xslt /app/output/apirun.xml
+
+exit $BOMBED
