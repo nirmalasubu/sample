42a16071507275e0d8950b249a6119da31356ba8
Nirmala Subramaniam <nirmalasubu@yahoo.com>
2017-06-13 07:20:55 -0400

    ODTD-664  edit destination properties

--- OnDemandTools.Web/App/Components/Destinations/AddEditDestinationProperties.jsx
+++ OnDemandTools.Web/App/Components/Destinations/AddEditDestinationProperties.jsx
@@ -1,29 +1,22 @@
 import React from 'react';
+import { Tabs, Checkbox, Tab, Grid, Row, Col, InputGroup, Radio, Form, ControlLabel, FormGroup, FormControl, Button } from 'react-bootstrap';
 import DestinationPropertiesForm from 'Components/Destinations/DestinationPropertiesForm';
+
+
 
 class AddEditDestinationProperties extends React.Component {
 
   constructor(props) {
-    super(props);
+      super(props);
+      this.state = ({
+          destinationProperties:{}
+      });
   }
-  componentDidMount() {
-
+    componentDidMount() {
+        
+      this.setState({
+          destinationProperties:this.props.data});
   }
-
-  textFormat(val, row) {
-    return <div>{val}</div>
-  }
-
-  isExpandableRow(row) {
-    return true;
-  }
-
-  expandComponent(row) {
-    return (
-      <DestinationPropertiesForm data={row} />
-    );
-  }
-
 
   actionFormat(val, rowData) {
     return (
@@ -38,22 +31,61 @@ class AddEditDestinationProperties extends React.Component {
       </div>
     );
   }
+              handlePropertyNameChange(event) {
+                  var model = this.state.destinationProperties;
+                  model.properties[event.target.id].name = event.target.value;
+                  this.setState({ destinationProperties: model });
+                 
+              }
 
-  render() {
+              handlePropertyValueChange(event) {
+                  var model = this.state.destinationProperties;
+                  model.properties[event.target.id].value = event.target.value;
+                  this.setState({ destinationProperties: model });
+                 
+              }
+
+              render() {
+                  var row;
+                  if (Object.keys(this.state.destinationProperties).length != 0 && this.state.destinationProperties != Object) 
+                  {
+                     
+                      row = this.state.destinationProperties.properties.map(function (item, index) {
+                          return (<div class="destination-properties-RowMargin"><Row >
+                         <Col sm={3} > <input type="text" id={index} defaultValue={item.name} onChange={this.handlePropertyNameChange.bind(this)} /> </Col>
+                         <Col sm={3} ><input type="text"  id={index} defaultValue={item.value}  onChange={this.handlePropertyValueChange.bind(this)} /></Col>
+                         <Col sm={3} ><label class="destination-properties-label">Filter</label></Col>
+                         <Col sm={3} > <button class="btn-link" title="Edit Properties" >
+                                      <i class="fa fa-pencil-square-o"></i>
+                                    </button>
+
+                                    <button class="btn-link" title="Delete Properties" >
+                                      <i class="fa fa-trash"></i></button>
+                           </Col>
+                         </Row></div>);
+                             }.bind(this));
+                             }
     return (
       <div>
-        <BootstrapTable
-          data={this.props.data.properties}
-          striped hover
-          expandableRow={this.isExpandableRow}
-          expandComponent={this.expandComponent}>
-          <TableHeaderColumn isKey dataField="name" dataSort={true} dataFormat={this.textFormat.bind(this)}>Name</TableHeaderColumn>
-          <TableHeaderColumn dataField="value" dataSort={true} dataFormat={this.textFormat.bind(this)}>Value</TableHeaderColumn>
-          <TableHeaderColumn width="100px" dataField="value" dataSort={false} dataFormat={this.actionFormat.bind(this)}>Actions</TableHeaderColumn>
-        </BootstrapTable>
+            <div>
+                    <button class="destination-properties-addnew" title="Add New"  onClick={(event) => this.openCreateNewQueueModel(event)}>
+                        Add  New
+                    </button>
+               </div>
+          <Grid fluid={true}>
+              <Row>
+                  <Col sm={3} ><label class="destination-properties-label">Name</label></Col>
+                      <Col sm={3} ><label class="destination-properties-label">Value</label></Col>
+                          <Col sm={3} ><label class="destination-properties-label">Filter</label></Col>
+                              <Col sm={3} ><label class="destination-properties-label">Actions</label></Col>
+              </Row>
+              {row}
+              </Grid>
+
+       
       </div>
     )
-  }
-}
+                              }
+                    }
 
 export default AddEditDestinationProperties
