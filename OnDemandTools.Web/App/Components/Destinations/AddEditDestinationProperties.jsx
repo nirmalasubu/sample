7fd7a482ea7265b012302bd8c2f4d08f10bfe51f
Shanmuga Sivagnanam <shanmuga.sundaram@turner.com>
2017-06-14 11:42:57 -0400

    ODT 722 Properties Filters added

--- OnDemandTools.Web/App/Components/Destinations/AddEditDestinationProperties.jsx
+++ OnDemandTools.Web/App/Components/Destinations/AddEditDestinationProperties.jsx
@@ -1,90 +1,102 @@
 import React from 'react';
-import { Tabs, Checkbox, Tab, Grid, Row, Col, InputGroup, Radio, Form, ControlLabel, FormGroup, FormControl, Button,OverlayTrigger,Popover } from 'react-bootstrap';
-import DestinationPropertiesForm from 'Components/Destinations/DestinationPropertiesForm';
+import { Tabs, Checkbox, Tab, Grid, Row, Col, InputGroup, Radio, Form, ControlLabel, FormGroup, FormControl, Button, OverlayTrigger, Popover } from 'react-bootstrap';
+import DestinationPropertiesFilter from 'Components/Destinations/DestinationPropertiesFilter';
 
 
 
 class AddEditDestinationProperties extends React.Component {
 
   constructor(props) {
-      super(props);
-      this.state = ({
-          destinationProperties:{}
-      });
-  }
-    componentDidMount() {
-          
-      this.setState({
-          destinationProperties:this.props.data});
+    super(props);
+    this.state = ({
+      destinationProperties: {},
+      showAddEditPropertiesFilter: false,
+      destinationPropertiesRow: {}
+    });
   }
 
+  componentDidMount() {
+    this.setState({
+      destinationProperties: this.props.data
+    });
+  }
 
-              handlePropertyNameChange(event) {
-                  var model = this.state.destinationProperties;
-                  model.properties[event.target.id].name = event.target.value;
-                  this.setState({ destinationProperties: model });
-                  this.props.data=this.state.destinationProperties;
-                 
-              }
+  openPropertiesFilter(row) {
+    this.setState({ showAddEditPropertiesFilter: true, destinationPropertiesRow: row });
+  }
 
-              handlePropertyValueChange(event) {
-                  var model = this.state.destinationProperties;
-                  model.properties[event.target.id].value = event.target.value;
-                  this.setState({ destinationProperties: model });
-                  this.props.data=this.state.destinationProperties;
-                 
-              }
+  closePropertiesFilter() {
+    this.setState({ showAddEditPropertiesFilter: false });    
+  }
 
-              render() {
-                  let row =null;
-                  if (Object.keys(this.state.destinationProperties).length != 0 && this.state.destinationProperties != Object) 
-                  {
-                     
-                      row = this.state.destinationProperties.properties.map(function (item, index) {
-                         
-                          return (<div class="destination-properties-RowMargin"><Row >
-                         <Col sm={3} > <input type="text" id={index} defaultValue={item.name} onChange={this.handlePropertyNameChange.bind(this)} /> </Col>
-                         <Col sm={3} ><input type="text"  id={index} defaultValue={item.value}  onChange={this.handlePropertyValueChange.bind(this)} /></Col>
-                         <Col sm={4} >
-                                {item.brands.map(function(name, index){
-                                    var path="images/brands/"+name+".gif"
-                                    return ( <img src={path} />);
-                                })}
-                     
-                                {item.titles.map(function(title, index){
-                                     return (<span>{title.name}</span>  );
-                                })}
-                         </Col>
-                         <Col sm={2} > 
-                             <button class="btn-link" title="Edit Properties" >
-                                      <i class="fa fa-pencil-square-o"></i>
-                              </button>
-                              <button class="btn-link" title="Delete Properties" >
-                                      <i class="fa fa-trash"></i></button>
-                           </Col>
-                         </Row></div>);
-                             }.bind(this));
-                        }
-                             
-                        return (
-                          <div>
-                          <div>
-                        <button class="destination-properties-addnew" title="Add New"  onClick={(event) => this.openCreateNewQueueModel(event)}>
-                        Add  New
+
+  handlePropertyNameChange(event) {
+    var model = this.state.destinationProperties;
+    model.properties[event.target.id].name = event.target.value;
+    this.setState({ destinationProperties: model });
+    this.props.data = this.state.destinationProperties;
+
+  }
+
+  handlePropertyValueChange(event) {
+    var model = this.state.destinationProperties;
+    model.properties[event.target.id].value = event.target.value;
+    this.setState({ destinationProperties: model });
+    this.props.data = this.state.destinationProperties;
+
+  }
+
+  render() {
+    let row = null;
+    if (Object.keys(this.state.destinationProperties).length != 0 && this.state.destinationProperties != Object) {
+
+      row = this.state.destinationProperties.properties.map(function (item, index) {
+
+        return (<div class="destination-properties-RowMargin"><Row >
+          <Col sm={3} > <input type="text" id={index} defaultValue={item.name} onChange={this.handlePropertyNameChange.bind(this)} /> </Col>
+          <Col sm={3} ><input type="text" id={index} defaultValue={item.value} onChange={this.handlePropertyValueChange.bind(this)} /></Col>
+          <Col sm={4} >
+            {item.brands.map(function (name, index) {
+              var path = "images/brands/" + name + ".gif"
+              return (<img src={path} />);
+            })}
+
+            {item.titles.map(function (title, index) {
+              return (<span>{title.name}</span>);
+            })}
+          </Col>
+          <Col sm={2} >
+            <button class="btn-link" title="Edit Filter" onClick={(event) => this.openPropertiesFilter(row, event)} >
+              <i class="fa fa-pencil-square-o"></i>
+            </button>
+            <button class="btn-link" title="Delete Property" >
+              <i class="fa fa-trash"></i></button>
+          </Col>
+        </Row></div>);
+      }.bind(this));
+    }
+
+    return (
+      <div>
+        <div>
+          <button class="destination-properties-addnew" title="Add New" onClick={(event) => this.openCreateNewQueueModel(event)}>
+            Add  New
                         </button>
-                        </div>
-                <Grid fluid={true}>
-              <Row>
-                   <Col sm={3} ><label class="destination-properties-label">Name</label></Col>
-                   <Col sm={3} ><label class="destination-properties-label">Value</label></Col>
-                   <Col sm={4} ><label class="destination-properties-label">Filter</label></Col>
-                   <Col sm={2} ><label class="destination-properties-label">Actions</label></Col>
-              </Row>
-             {row}
-              </Grid>
+        </div>
+        <Grid fluid={true}>
+          <Row>
+            <Col sm={3} ><label class="destination-properties-label">Name</label></Col>
+            <Col sm={3} ><label class="destination-properties-label">Value</label></Col>
+            <Col sm={4} ><label class="destination-properties-label">Filter</label></Col>
+            <Col sm={2} ><label class="destination-properties-label">Actions</label></Col>
+          </Row>
+          {row}
+        </Grid>
+
+        <DestinationPropertiesFilter data={this.state} handleClose={this.closePropertiesFilter.bind(this)} />
       </div>
     )
-              }
-                    }
+  }
+}
 
 export default AddEditDestinationProperties
---
+++ OnDemandTools.Web/App/Components/Destinations/DestinationPropertiesFilter.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import PageHeader from 'Components/Common/PageHeader';
+import { Checkbox, Grid, Row, Col, InputGroup, Radio, Form, ControlLabel, FormGroup, FormControl, Button } from 'react-bootstrap';
+import { Modal } from 'react-bootstrap';
+import { ModalDialogue } from 'react-bootstrap';
+import { ModalBody } from 'react-bootstrap';
+import { ModalFooter } from 'react-bootstrap';
+import { ModalHeader } from 'react-bootstrap';
+import { ModalTitle } from 'react-bootstrap';
+
+class DestinationPropertiesForm extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+  componentDidMount() {
+
+  }
+
+  resetForm() {
+    
+  }
+
+  render() {
+    return (
+      <Modal className="destinationPropertiesFilterModel" bsSize="large" 
+        backdrop="static"
+        onEntering={this.resetForm.bind(this)}
+        show={this.props.data.showAddEditPropertiesFilter}
+        onHide={this.props.handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>
+            <div>Filters</div>
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div>
+            <h1>Properties</h1>
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={this.props.handleClose}>Close</Button>
+          <Button className="btn btn-primary btn-large">Save</Button>
+        </Modal.Footer>
+      </Modal>
+    )
+  }
+}
+
+export default DestinationPropertiesForm
