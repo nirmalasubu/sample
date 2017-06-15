ca0de7ca3d4a6aab46b26d831c933ae266f9adc2
Shanmuga Sivagnanam <shanmuga.sundaram@turner.com>
2017-06-15 16:01:49 -0400

    ODT 722 code formatted

--- OnDemandTools.Web/App/Components/Destinations/AddEditDestinationProperties.jsx
+++ OnDemandTools.Web/App/Components/Destinations/AddEditDestinationProperties.jsx
@@ -11,22 +11,22 @@ class AddEditDestinationProperties extends React.Component {
     this.state = ({
       destinationDetails: {},
       showAddEditPropertiesFilter: false,
-      showPropertiesDeleteModal:false,
+      showPropertiesDeleteModal: false,
       destinationPropertiesRow: {},
-      propertyIndexToRemove:-1
+      propertyIndexToRemove: -1
     });
   }
 
-    componentDidMount() {
+  componentDidMount() {
     this.setState({
-        destinationDetails: this.props.data
+      destinationDetails: this.props.data
     });
   }
 
   componentWillMount() {
-      this.setState({
-          destinationDetails: this.props.data
-      });
+    this.setState({
+      destinationDetails: this.props.data
+    });
   }
 
   openPropertiesFilter(row) {
@@ -34,41 +34,39 @@ class AddEditDestinationProperties extends React.Component {
   }
 
   closePropertiesFilter() {
-    this.setState({ showAddEditPropertiesFilter: false });    
+    this.setState({ showAddEditPropertiesFilter: false });
   }
 
-  RemovePropertiesModel(value)
-  {
-      var array=[];
-      array = this.state.destinationDetails;
-      array.properties.splice(value, 1);
-      this.setState({destinationDetails: array });
-      this.props.data = this.state.destinationDetails;
-      this.setState({ showPropertiesDeleteModal: false});
+  RemovePropertiesModel(value) {
+    var array = [];
+    array = this.state.destinationDetails;
+    array.properties.splice(value, 1);
+    this.setState({ destinationDetails: array });
+    this.props.data = this.state.destinationDetails;
+    this.setState({ showPropertiesDeleteModal: false });
 
   }
-  AddNewProperty()
-  {
-      var newProperty={name:"",value:"", brands:[],titleIds:[],seriesIds:[],titles:[]}
-      var array=[];
-      array = this.state.destinationDetails;
-      array.properties.push(newProperty);
-      array.properties.reverse();
-      this.setState({destinationDetails: array });
-      this.props.data = this.state.destinationDetails;
-      
+  AddNewProperty() {
+    var newProperty = { name: "", value: "", brands: [], titleIds: [], seriesIds: [], titles: [] }
+    var array = [];
+    array = this.state.destinationDetails;
+    array.properties.push(newProperty);
+    array.properties.reverse();
+    this.setState({ destinationDetails: array });
+    this.props.data = this.state.destinationDetails;
+
   }
 
   openPropertiesDeleteModel(item) {
-      this.setState({ showPropertiesDeleteModal: true, propertyIndexToRemove: item });
+    this.setState({ showPropertiesDeleteModal: true, propertyIndexToRemove: item });
   }
 
   closePropertiesDeleteModel() {
-      this.setState({ showPropertiesDeleteModal: false });
+    this.setState({ showPropertiesDeleteModal: false });
   }
 
   handlePropertyNameChange(event) {
-      var model = this.state.destinationDetails;
+    var model = this.state.destinationDetails;
     model.properties[event.target.id].name = event.target.value;
     this.setState({ destinationDetails: model });
     this.props.data = this.state.destinationDetails;
@@ -76,7 +74,7 @@ class AddEditDestinationProperties extends React.Component {
   }
 
   handlePropertyValueChange(event) {
-      var model = this.state.destinationDetails;
+    var model = this.state.destinationDetails;
     model.properties[event.target.id].value = event.target.value;
     this.setState({ destinationDetails: model });
     this.props.data = this.state.destinationDetails;
@@ -86,42 +84,38 @@ class AddEditDestinationProperties extends React.Component {
 
 
   render() {
-      let row = null;
-      if (Object.keys(this.state.destinationDetails).length != 0 && this.state.destinationDetails != Object) {
-          if(Object.keys(this.state.destinationDetails.properties).length !== 0 && this.state.destinationDetails.properties != Object)
-          {
-              row = this.state.destinationDetails.properties.map(function (item, index) {
-                  return (<div class="destination-properties-RowMargin"><Row >
-                 <Col sm={3} > <input type="text" id={index} value={item.name} onChange={this.handlePropertyNameChange.bind(this)} /> </Col>
-                 <Col sm={3} ><input type="text" id={index} value={item.value} onChange={this.handlePropertyValueChange.bind(this)} /></Col>
-                 <Col sm={4} >
-                   {item.brands.map(function (name, index) {
-                       var path = "images/brands/" + name + ".gif"
-                       return (<img src={path} />);
-      })}
+    let row = null;
+    if (Object.keys(this.state.destinationDetails).length != 0 && this.state.destinationDetails != Object) {
+      if (Object.keys(this.state.destinationDetails.properties).length !== 0 && this.state.destinationDetails.properties != Object) {
+        row = this.state.destinationDetails.properties.map(function (item, index) {
+          return (<div class="destination-properties-RowMargin"><Row >
+            <Col sm={3} > <input type="text" id={index} value={item.name} onChange={this.handlePropertyNameChange.bind(this)} /> </Col>
+            <Col sm={3} ><input type="text" id={index} value={item.value} onChange={this.handlePropertyValueChange.bind(this)} /></Col>
+            <Col sm={4} >
+              {item.brands.map(function (name, index) {
+                var path = "images/brands/" + name + ".gif"
+                return (<img src={path} />);
+              })}
 
-    {item.titles.map(function (title, index) {
-        return (<span>{title.name}</span>);
-    })}
-              </Col>
-              <Col sm={2} >
-                <button class="btn-link" title="Edit Filter" onClick={(event) => this.openPropertiesFilter(item, event)} >  
-                  <i class="fa fa-pencil-square-o"></i>
-                </button>
-                <button class="btn-link" title="Delete Property" onClick={(event) => this.openPropertiesDeleteModel(index, event)} >
-                  <i class="fa fa-trash"></i></button>
-              </Col>
-            </Row></div>);
-                  }.bind(this));
-                  }
-                else
-                {
-                row =<Row><Col sm={12}><p>No properties available</p></Col></Row>
-               
-                }
-                }
-         
-         
+              {item.titles.map(function (title, index) {
+                return (<span>{title.name}</span>);
+              })}
+            </Col>
+            <Col sm={2} >
+              <button class="btn-link" title="Edit Filter" onClick={(event) => this.openPropertiesFilter(item, event)} >
+                <i class="fa fa-pencil-square-o"></i>
+              </button>
+              <button class="btn-link" title="Delete Property" onClick={(event) => this.openPropertiesDeleteModel(index, event)} >
+                <i class="fa fa-trash"></i></button>
+            </Col>
+          </Row></div>);
+        }.bind(this));
+      }
+      else {
+        row = <Row><Col sm={12}><p>No properties available</p></Col></Row>
+
+      }
+    }
 
     return (
       <div>
@@ -131,20 +125,20 @@ class AddEditDestinationProperties extends React.Component {
                         </button>
         </div>
         <div >
-        <Grid fluid={true}>
-          <Row>
-            <Col sm={3} ><label class="destination-properties-label">Name</label></Col>
-            <Col sm={3} ><label class="destination-properties-label">Value</label></Col>
-            <Col sm={4} ><label class="destination-properties-label">Filter</label></Col>
-            <Col sm={2} ><label class="destination-properties-label">Actions</label></Col>
-          </Row>
-          <div class="destination-height">
-          {row}
-              </div>
-        </Grid>
+          <Grid fluid={true}>
+            <Row>
+              <Col sm={3} ><label class="destination-properties-label">Name</label></Col>
+              <Col sm={3} ><label class="destination-properties-label">Value</label></Col>
+              <Col sm={4} ><label class="destination-properties-label">Filter</label></Col>
+              <Col sm={2} ><label class="destination-properties-label">Actions</label></Col>
+            </Row>
+            <div class="destination-height">
+              {row}
             </div>
-       <DestinationPropertiesFilter data={this.state} handleClose={this.closePropertiesFilter.bind(this)} />
-        <RemovePropertiesModal data={this.state} handleClose={this.closePropertiesDeleteModel.bind(this)}  handleRemoveAndClose={this.RemovePropertiesModel.bind(this)} />
+          </Grid>
+        </div>
+        <DestinationPropertiesFilter data={this.state} handleClose={this.closePropertiesFilter.bind(this)} />
+        <RemovePropertiesModal data={this.state} handleClose={this.closePropertiesDeleteModel.bind(this)} handleRemoveAndClose={this.RemovePropertiesModel.bind(this)} />
       </div>
     )
   }
