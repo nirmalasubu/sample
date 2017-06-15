03c05a6b2963ebf7cc49d6f2414f8b207296dec3
Nirmala Subramaniam <nirmalasubu@yahoo.com>
2017-06-15 07:50:40 -0400

    ODT-664 Remove Property functionality added

--- OnDemandTools.Web/App/Components/Destinations/AddEditDestinationProperties.jsx
+++ OnDemandTools.Web/App/Components/Destinations/AddEditDestinationProperties.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Tabs, Checkbox, Tab, Grid, Row, Col, InputGroup, Radio, Form, ControlLabel, FormGroup, FormControl, Button, OverlayTrigger, Popover } from 'react-bootstrap';
 import DestinationPropertiesFilter from 'Components/Destinations/DestinationPropertiesFilter';
-
+import RemovePropertiesModal from 'Components/Destinations/RemovePropertiesModal';
 
 
 class AddEditDestinationProperties extends React.Component {
@@ -9,16 +9,24 @@ class AddEditDestinationProperties extends React.Component {
   constructor(props) {
     super(props);
     this.state = ({
-      destinationProperties: {},
+      destinationDetails: {},
       showAddEditPropertiesFilter: false,
-      destinationPropertiesRow: {}
+      showPropertiesDeleteModal:false,
+      destinationPropertiesRow: {},
+      propertyIndexToRemove:-1
     });
   }
 
-  componentDidMount() {
+    componentDidMount() {
     this.setState({
-      destinationProperties: this.props.data
+        destinationDetails: this.props.data
     });
+  }
+
+  componentWillMount() {
+      this.setState({
+          destinationDetails: this.props.data
+      });
   }
 
   openPropertiesFilter(row) {
@@ -29,52 +37,80 @@ class AddEditDestinationProperties extends React.Component {
     this.setState({ showAddEditPropertiesFilter: false });    
   }
 
+  RemovePropertiesModel(value)
+  {
+      var array=[];
+      array = this.state.destinationDetails;
+      array.properties.splice(value, 1);
+      this.setState({destinationDetails: array });
+      this.props.data = this.state.destinationDetails;
+      this.setState({ showPropertiesDeleteModal: false});
+
+  }
+
+  openPropertiesDeleteModel(item) {
+      this.setState({ showPropertiesDeleteModal: true, propertyIndexToRemove: item });
+  }
+
+  closePropertiesDeleteModel() {
+      this.setState({ showPropertiesDeleteModal: false });
+  }
 
   handlePropertyNameChange(event) {
-    var model = this.state.destinationProperties;
+      var model = this.state.destinationDetails;
     model.properties[event.target.id].name = event.target.value;
-    this.setState({ destinationProperties: model });
-    this.props.data = this.state.destinationProperties;
+    this.setState({ destinationDetails: model });
+    this.props.data = this.state.destinationDetails;
 
   }
 
   handlePropertyValueChange(event) {
-    var model = this.state.destinationProperties;
+      var model = this.state.destinationDetails;
     model.properties[event.target.id].value = event.target.value;
-    this.setState({ destinationProperties: model });
-    this.props.data = this.state.destinationProperties;
+    this.setState({ destinationDetails: model });
+    this.props.data = this.state.destinationDetails;
 
   }
 
+
+
   render() {
-    let row = null;
-    if (Object.keys(this.state.destinationProperties).length != 0 && this.state.destinationProperties != Object) {
+      let row = null;
+      if (Object.keys(this.state.destinationDetails).length != 0 && this.state.destinationDetails != Object) {
+          if(Object.keys(this.state.destinationDetails.properties).length !== 0 && this.state.destinationDetails.properties != Object)
+          {
+              row = this.state.destinationDetails.properties.map(function (item, index) {
+                  return (<div class="destination-properties-RowMargin"><Row >
+                 <Col sm={3} > <input type="text" id={index} value={item.name} onChange={this.handlePropertyNameChange.bind(this)} /> </Col>
+                 <Col sm={3} ><input type="text" id={index} value={item.value} onChange={this.handlePropertyValueChange.bind(this)} /></Col>
+                 <Col sm={4} >
+                   {item.brands.map(function (name, index) {
+                       var path = "images/brands/" + name + ".gif"
+                       return (<img src={path} />);
+      })}
 
-      row = this.state.destinationProperties.properties.map(function (item, index) {
-
-        return (<div class="destination-properties-RowMargin"><Row >
-          <Col sm={3} > <input type="text" id={index} defaultValue={item.name} onChange={this.handlePropertyNameChange.bind(this)} /> </Col>
-          <Col sm={3} ><input type="text" id={index} defaultValue={item.value} onChange={this.handlePropertyValueChange.bind(this)} /></Col>
-          <Col sm={4} >
-            {item.brands.map(function (name, index) {
-              var path = "images/brands/" + name + ".gif"
-              return (<img src={path} />);
-            })}
-
-            {item.titles.map(function (title, index) {
-              return (<span>{title.name}</span>);
-            })}
-          </Col>
-          <Col sm={2} >
-            <button class="btn-link" title="Edit Filter" onClick={(event) => this.openPropertiesFilter(item, event)} >
-              <i class="fa fa-pencil-square-o"></i>
-            </button>
-            <button class="btn-link" title="Delete Property" >
-              <i class="fa fa-trash"></i></button>
-          </Col>
-        </Row></div>);
-      }.bind(this));
-    }
+    {item.titles.map(function (title, index) {
+        return (<span>{title.name}</span>);
+    })}
+              </Col>
+              <Col sm={2} >
+                <button class="btn-link" title="Edit Filter" onClick={(event) => this.openPropertiesFilter(item, event)} >  
+                  <i class="fa fa-pencil-square-o"></i>
+                </button>
+                <button class="btn-link" title="Delete Property" onClick={(event) => this.openPropertiesDeleteModel(index, event)} >
+                  <i class="fa fa-trash"></i></button>
+              </Col>
+            </Row></div>);
+                  }.bind(this));
+                  }
+                else
+                {
+                row =<Row><Col sm={12}><p>No properties available</p></Col></Row>
+               
+                }
+                }
+         
+         
 
     return (
       <div>
@@ -83,6 +119,7 @@ class AddEditDestinationProperties extends React.Component {
             Add  New
                         </button>
         </div>
+        <div >
         <Grid fluid={true}>
           <Row>
             <Col sm={3} ><label class="destination-properties-label">Name</label></Col>
@@ -90,10 +127,13 @@ class AddEditDestinationProperties extends React.Component {
             <Col sm={4} ><label class="destination-properties-label">Filter</label></Col>
             <Col sm={2} ><label class="destination-properties-label">Actions</label></Col>
           </Row>
+          <div class="destination-height">
           {row}
+              </div>
         </Grid>
-
-        <DestinationPropertiesFilter data={this.state} handleClose={this.closePropertiesFilter.bind(this)} />
+            </div>
+       <DestinationPropertiesFilter data={this.state} handleClose={this.closePropertiesFilter.bind(this)} />
+        <RemovePropertiesModal data={this.state} handleClose={this.closePropertiesDeleteModel.bind(this)}  handleRemoveAndClose={this.RemovePropertiesModel.bind(this)} />
       </div>
     )
   }
---
+++ OnDemandTools.Web/App/Components/Destinations/RemovePropertiesModal.jsx
@@ -0,0 +1,57 @@
+﻿import React from 'react';
+import { connect } from 'react-redux';
+import $ from 'jquery';
+import { Modal } from 'react-bootstrap';
+import { ModalDialogue } from 'react-bootstrap';
+import { ModalBody } from 'react-bootstrap';
+import { ModalFooter } from 'react-bootstrap';
+import { ModalHeader } from 'react-bootstrap';
+import { ModalTitle } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
+
+
+class RemovePropertiesModal extends React.Component
+{
+    constructor(props) {
+        super(props);
+
+        this.state = ({
+            message: ""
+        });
+    }
+
+    onOpenModal(){        
+        this.setState({
+            message:" Would you like to Remove the property?"
+        });
+    }
+
+    onContinue(){
+        this.props.handleClose();
+        this.props.handleRemoveAndClose(this.props.data.propertyIndexToRemove);
+    }
+
+    render(){
+
+        return (
+           <Modal show={this.props.data.showPropertiesDeleteModal} onEntering={this.onOpenModal.bind(this)} onHide={this.props.handleClose}> 
+                <Modal.Header closeButton>
+                    <Modal.Title>
+           Warning !
+                    </Modal.Title>
+                </Modal.Header>        
+                <Modal.Body>
+           {
+                        <p>{this.state.message}</p>
+           }
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button  onClick={this.props.handleClose}>NO</Button>  
+                    <Button bsStyle="primary" onClick={(event) => this.onContinue(event)}>YES</Button>
+                </Modal.Footer>
+           </Modal>
+        )
+           }
+}
+
+export default RemovePropertiesModal;
