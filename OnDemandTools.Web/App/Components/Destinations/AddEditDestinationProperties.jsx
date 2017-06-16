e7e3913d98b34ebcd9c64712da9243f5de954722
Nirmala Subramaniam <nirmalasubu@yahoo.com>
2017-06-16 07:56:41 -0400

    ODT-664 css updated

--- OnDemandTools.Web/App/Components/Destinations/AddEditDestinationProperties.jsx
+++ OnDemandTools.Web/App/Components/Destinations/AddEditDestinationProperties.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Tabs, Checkbox, Tab, Grid, Row, Col, InputGroup, Radio, Form, ControlLabel, FormGroup, FormControl, Button, OverlayTrigger, Popover } from 'react-bootstrap';
+import { Tabs, Checkbox, Tab, Grid, Row, Col, InputGroup, Radio, Form, ControlLabel, FormGroup, FormControl, Button, OverlayTrigger, Popover,Collapse,Well } from 'react-bootstrap';
 import DestinationPropertiesFilter from 'Components/Destinations/DestinationPropertiesFilter';
 import RemovePropertiesModal from 'Components/Destinations/RemovePropertiesModal';
 
@@ -14,7 +14,9 @@ class AddEditDestinationProperties extends React.Component {
       showPropertiesDeleteModal:false,
       isPropertyNameRequired:false,
       destinationPropertiesRow: {},
-      propertyIndexToRemove: -1
+      propertyIndexToRemove:-1,
+      titleopen:false,
+      imageopen:false
     });
   }
 
@@ -113,8 +115,15 @@ class AddEditDestinationProperties extends React.Component {
           if(Object.keys(this.state.destinationDetails.properties).length !== 0 && this.state.destinationDetails.properties != Object)
           {
               row = this.state.destinationDetails.properties.map(function (item, index) {
-                 var nameValidation=item.name?"":"error"
-                  return (<div class="destination-properties-RowMargin"><Row >
+                  var nameValidation=item.name?"":"error"
+                  var titleName=[]
+                  item.titles.map(function (title, index) {titleName.push(title.name)})
+                  var  firstTitle=titleName[0]
+                  var titlestring=titleName.toString()
+                 
+                 
+
+                  return (<Row >
                  <Form>
                  <Col sm={3} >
                       <FormGroup controlId="Name" validationState={nameValidation}>
@@ -125,60 +134,69 @@ class AddEditDestinationProperties extends React.Component {
                      <FormControl type="text" id={index} value={item.value} ref="Value"  placeholder="Value"  onChange={this.handlePropertyValueChange.bind(this)}  />
                      </FormGroup></Col>
                   </Form>
-                 <Col sm={4} >
-                   {item.brands.map(function (name, index) {
-                       var path = "images/brands/" + name + ".gif"
-                       return (<img src={path} />);
-                     })}
-
-                {item.titles.map(function (title, index) {
-                    return (<span>{title.name}</span>);
-                 })}
-              </Col>
-              <Col sm={2} >
-                <button class="btn-link" title="Edit Filter" onClick={(event) => this.openPropertiesFilter(item, event)} >  
-                  <i class="fa fa-pencil-square-o"></i>
-                </button>
-                <button class="btn-link" title="Delete Property" onClick={(event) => this.openPropertiesDeleteModel(index, event)} >
-                  <i class="fa fa-trash"></i></button>
-              </Col>
-            </Row></div>);
-                  }.bind(this));
-                  }
-                else
-                {
-                row =<Row><Col sm={12}><p>No properties available</p></Col></Row>
+                 <Col sm={2} >
+                    
+                {item.brands.map(function (name, index) {
+                    var path = "images/brands/" + name + ".gif"
+                    return (<img src={path} />);
+                })}
                
-                }
-                }
+                   
+                </Col>
+                <Col sm={2} >
+          <div onClick={ ()=> this.setState({ titleopen: !this.state.titleopen })}>
+                {firstTitle} 
+              </div>
+              <Collapse in={this.state.titleopen}>
+                <div>
+                {titlestring}
+                 </div> 
+              </Collapse>
+                          </Col>
+                          <Col sm={2} >
+                            <button class="btn-link" title="Edit Filter" onClick={(event) => this.openPropertiesFilter(item, event)} >  
+                              <i class="fa fa-pencil-square-o"></i>
+                            </button>
+                            <button class="btn-link" title="Delete Property" onClick={(event) => this.openPropertiesDeleteModel(index, event)} >
+                              <i class="fa fa-trash"></i></button>
+                          </Col>
+                        </Row>)
+                              }.bind(this));
+                              }
+                              else
+                              {
+                              row =<Row><Col sm={12}><p> No properties available</p></Col></Row>
+                              }
+               
+                              }
          
          
 
-    return (
-      <div>
-        <div>
-          <button class="destination-properties-addnew" title="Add New" onClick={(event) => this.AddNewProperty(event)}>
-            Add  New
-                        </button>
-        </div>
-        <div >
-          <Grid fluid={true}>
-            <Row>
-              <Col sm={3} ><label class="destination-properties-label">Name</label></Col>
-              <Col sm={3} ><label class="destination-properties-label">Value</label></Col>
-              <Col sm={4} ><label class="destination-properties-label">Filter</label></Col>
-              <Col sm={2} ><label class="destination-properties-label">Actions</label></Col>
-            </Row>
-            <div class="destination-height">
-              {row}
-            </div>
-          </Grid>
-        </div>
-        <DestinationPropertiesFilter data={this.state} handleClose={this.closePropertiesFilter.bind(this)} />
-        <RemovePropertiesModal data={this.state} handleClose={this.closePropertiesDeleteModel.bind(this)} handleRemoveAndClose={this.RemovePropertiesModel.bind(this)} />
-      </div>
-    )
-  }
-}
+                  return (
+                    <div>
+                      <div>
+                        <button class="destination-properties-addnew btn" title="Add New" onClick={(event) => this.AddNewProperty(event)}>
+                                Add  New
+                                            </button>
+                            </div>
+                            <div >
+                            <Grid fluid={true}>
+                      <Row>
+                        <Col sm={3} ><label class="destination-properties-label">Name</label></Col>
+                        <Col sm={3} ><label class="destination-properties-label">Value</label></Col>
+                        <Col sm={4} ><label class="destination-properties-label  destination-properties-filtermargin">Filter</label></Col>
+                        <Col sm={2} ><label class="destination-properties-label destination-properties-actionmargin">Actions</label></Col>
+                      </Row>
+                      <div class="destination-height">
+                        {row}
+                          </div>
+                    </Grid>
+                        </div>
+                   <DestinationPropertiesFilter data={this.state} handleClose={this.closePropertiesFilter.bind(this)} />
+                    <RemovePropertiesModal data={this.state} handleClose={this.closePropertiesDeleteModel.bind(this)}  handleRemoveAndClose={this.RemovePropertiesModel.bind(this)} />
+                  </div>
+                )
+                  }
+                            }
 
-export default AddEditDestinationProperties
+            export default AddEditDestinationProperties
