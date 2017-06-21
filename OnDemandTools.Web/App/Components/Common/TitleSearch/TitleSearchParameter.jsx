7a741a274624ffe6132b61821937180d403e5705
Shanmuga Sivagnanam <shanmuga.sundaram@turner.com>
2017-06-21 13:37:26 -0400

    ODT-722 Drop down selection included

--- OnDemandTools.Web/App/Components/Common/TitleSearch/TitleSearchParameter.jsx
+++ OnDemandTools.Web/App/Components/Common/TitleSearch/TitleSearchParameter.jsx
@@ -4,6 +4,17 @@ import { FormGroup } from 'react-bootstrap';
 class TitleSearchParameter extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            contentValue: ""
+        };
+    }
+
+    handleSelectChange(value){
+        if(value == undefined)
+            value ="";
+        this.setState({
+            contentValue: value
+        });
     }
 
     render() {
@@ -23,6 +34,8 @@ class TitleSearchParameter extends React.Component {
                 <Select simpleValue className="destination-select-control" options={options}
                     clearable={true}
                     searchable={true}
+                    value={this.state.contentValue}
+                    onChange={this.handleSelectChange.bind(this)}
                     placeholder={this.props.name} />
             </FormGroup>
         )
