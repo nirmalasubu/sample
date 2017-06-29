2dde520d1e17d1798ed6f66572043d5f6eff52a5
Shanmuga Sivagnanam <shanmuga.sundaram@turner.com>
2017-06-29 11:13:41 -0400

    ODT 674 Title Name loaded

--- OnDemandTools.Web/App/Components/Common/TitleNameOverlay.jsx
+++ OnDemandTools.Web/App/Components/Common/TitleNameOverlay.jsx
@@ -1,14 +1,58 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { searchByTitleIds } from 'Actions/TitleSearch/TitleSearchActions';
 
+@connect((store) => {
+    return {        
+    };
+})
 class TitleNameOverlay extends React.Component {
     constructor(props) {
-        super(props);        
+        super(props);
+
+        this.state = {
+            titleIds: [],
+            titles: [],
+            isProcessing: false
+        }
+    }
+
+    fetchAndUpdateTitles() {
+
+        var titleIds= this.props.data;
+
+        let searchPromise = searchByTitleIds(titleIds);
+
+        searchPromise.then(message => {
+            this.setState({ titles: message, isProcessing: false });
+        })
+        searchPromise.catch(error => {
+            console.error(error);
+            this.setState({ titles: [], isProcessing: false });
+        });
+
+
+    }
+
+    componentDidMount() {
+       this.setState({
+            titlesIds: this.props.data,
+            isProcessing: true
+        });
+
+        this.fetchAndUpdateTitles();
     }
 
     render() {
+        if (this.state.isProcessing) {
+            return <div>Loading...</div>
+        }
+        else if (this.state.titles.length == 0) {
+            return <div></div>
+        }
         return (
             <div>
-                {this.props.data}
+                {this.state.titles[0].titleName}
             </div>
         )
     }
