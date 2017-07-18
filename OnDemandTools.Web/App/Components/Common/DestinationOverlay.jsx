af6ed0aed279bf4c7a46bbe47fc0d5175232e532
Shanmuga Sivagnanam <shanmuga.sundaram@turner.com>
2017-07-18 09:59:49 -0400

    ODT 740 Missing file added

---
+++ OnDemandTools.Web/App/Components/Common/DestinationOverlay.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Popover, OverlayTrigger, Button } from 'react-bootstrap';
+
+
+class DestinationOverlay extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        console.log(this.props.rows);
+        if(this.props.rows.length>1){
+            const popoverLeft = (
+                <Popover id="popover-positioned-left" title="Destinations">
+                    <div class="TitleOverlay-height"> {this.props.rows} </div>
+                </Popover>
+            );
+
+            return(
+                <OverlayTrigger trigger={['click', 'focus']} rootClose placement="left" overlay={popoverLeft}>
+                    <div className="cursorPointer" title="click to view more destinations">
+                    {this.props.rows[0]}{this.props.rows[1]} <i class="fa fa-ellipsis-h"></i>
+                    </div>
+                </OverlayTrigger>
+                );
+        }
+        else{
+            return (
+                <div>
+                    {this.props.rows}
+                </div>
+            );
+        }
+    }
+}
+
+export default DestinationOverlay
