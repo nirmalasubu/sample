ec491ed806e2c4fd5126876bca56f03c1f501b39
jjohn <dormantroot@gmail.com>
2017-08-07 21:57:36 -0400

    more temporary correction to context

--- OnDemandTools.Jobs/Helpers/JobContext.cs
+++ OnDemandTools.Jobs/Helpers/JobContext.cs
@@ -1,9 +1,16 @@
-﻿using OnDemandTools.Common.Configuration;
+﻿using System;
+using System.Security.Principal;
+using OnDemandTools.Common.Configuration;
 
 namespace OnDemandTools.Jobs.Helpers
 {
     public class JobContext : IApplicationContext
     {
+        public IIdentity GetHttpIdentity()
+        {
+            throw new NotImplementedException();
+        }
+
         /// <summary>
         /// Pass in actual user details to implement proper Authentication/Authorization
         /// </summary>
