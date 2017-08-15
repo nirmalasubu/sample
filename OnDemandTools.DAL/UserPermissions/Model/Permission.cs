e7a30d634a859f1510f26e143273bd134a573cbb
Shanmuga Sivagnanam <shanmuga.sundaram@turner.com>
2017-08-15 15:26:13 -0400

    Permissions initial idea

---
+++ OnDemandTools.DAL/UserPermissions/Model/Permission.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnDemandTools.DAL.UserPermissions.Model
+{
+    public class Permission
+    {
+        public Permission()
+        {
+
+        }
+
+        public bool CanRead { get; set; }
+
+        public bool CanAdd { get; set; }
+
+        public bool CanEdit { get; set;  }
+
+        public bool CanDelete { get; set; }
+    }
+}
---
+++ OnDemandTools.DAL/UserPermissions/Model/Portal.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnDemandTools.DAL.UserPermissions.Model
+{
+    public class Portal
+    {
+        public Portal()
+        {
+            Modules = new Dictionary<string, Permission>();
+            DeliveryQueue = new Dictionary<ObjectId, Permission>();
+        }
+
+        public bool IsAdmin { get; set; }
+
+        public DateTime LastLoginTime { get; set; }
+
+        public Dictionary<string, Permission> Modules { get; set; }
+
+        public Dictionary<ObjectId, Permission> DeliveryQueue { get; set; }
+    }
+}
