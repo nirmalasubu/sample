e2e4b5f113ff8ce7d25c6cf83f681bc059242f8d
Shanmuga Sivagnanam <shanmuga.sundaram@turner.com>
2017-08-28 13:55:47 -0400

    Content Tier filter update

--- OnDemandTools.Web/App/Components/ContentTiers/ContentTiersPage.jsx
+++ OnDemandTools.Web/App/Components/ContentTiers/ContentTiersPage.jsx
@@ -7,13 +7,9 @@ import ContentTierFilter from 'Components/ContentTiers/ContentTierFilter';
 import PageHeader from 'Components/Common/PageHeader';
 import 'react-notifications/lib/notifications.css';
 
-@connect((store) => {
-    // Retrieve filtered list of ContentTiers based on filterValue like contentTier name and product
-    // which are defined in Redux store
-    var filteredContentTierValues = getFilterVal(store.contentTiers, store.filterContentTier);
+@connect((store) => {   
     return {
-        contentTiers: store.contentTiers,
-        filteredContentTiers: (filteredContentTierValues != undefined ? filteredContentTierValues : store.products)
+        contentTiers: store.contentTiers
     };
 })
 class ContentTiersPage extends React.Component {
@@ -58,44 +54,44 @@ class ContentTiersPage extends React.Component {
         this.setState({
             filterValue: stateFilterValue
         });
-        this.props.dispatch(contentTierActions.filterContentTierSuccess(this.state.filterValue));
+
     }
 
     //called on the page load
     componentDidMount() {
-        this.props.dispatch(contentTierActions.filterContentTierSuccess(this.state.filterValue));
         this.props.dispatch(contentTierActions.fetchContentTiers());
         this.props.dispatch(productActions.fetchProducts());
         document.title = "ODT - Content Tiers";
     }
 
+    ///<summary>
+    // The goal of this function is to filter 'ContentTiers' (which is stored in Redux store)
+    // based on user provided filter criteria and return the refined 'contentTiers' list.
+    // If no filter criteria is provided then return the full 'contentTiers' list
+    ///</summary>
+    getFilterVal(contentTiers, filterVal) {
+        if (filterVal != undefined && filterVal.contentTierName != undefined) {
+            var contentTierName = filterVal.contentTierName.toLowerCase();
+            var product = filterVal.product.toLowerCase();
+            return (contentTiers.filter(obj => (contentTierName != "" ? obj.name.toLowerCase().indexOf(contentTierName) != -1 : true)
+                && (product != "" ? matchProducts(obj.products, product) : true)
+            ));
+        }
+        else
+            return contentTiers;
+    };
+
     render() {
+        var filteredContentTiers = this.getFilterVal(this.props.contentTiers, this.state.filterValue);
         return (
             <div>
                 <PageHeader pageName="Content Tiers" />
                 <ContentTierFilter updateFilter={this.handleFilterUpdate.bind(this)} />
-                <ContentTierTable RowData={this.props.filteredContentTiers} ColumnData={this.state.columns} KeyField={this.state.keyField} />
+                <ContentTierTable RowData={filteredContentTiers} ColumnData={this.state.columns} KeyField={this.state.keyField} />
             </div>
         )
     }
 }
-
-///<summary>
-// The goal of this function is to filter 'ContentTiers' (which is stored in Redux store)
-// based on user provided filter criteria and return the refined 'contentTiers' list.
-// If no filter criteria is provided then return the full 'contentTiers' list
-///</summary>
-const getFilterVal = (contentTiers, filterVal) => {
-    if (filterVal != undefined && filterVal.contentTierName != undefined) {
-        var contentTierName = filterVal.contentTierName.toLowerCase();
-        var product = filterVal.product.toLowerCase();
-        return (contentTiers.filter(obj => (contentTierName != "" ? obj.name.toLowerCase().indexOf(contentTierName) != -1 : true)
-            && (product != "" ? matchProducts(obj.products, product) : true)
-        ));
-    }
-    else
-        return contentTiers;
-};
 
 ///<summary>
 // returns  true  if any of the search value  product matches product list 
