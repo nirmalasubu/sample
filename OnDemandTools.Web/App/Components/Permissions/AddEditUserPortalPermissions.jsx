d62549de16f421053157c82c1d6ba3a7815bbc75
Nirmala Subramaniam <nirmalasubu@yahoo.com>
2017-08-29 12:28:52 -0400

    ODT-866 user css width and checkbox change updated

--- OnDemandTools.Web/App/Components/Permissions/AddEditUserPortalPermissions.jsx
+++ OnDemandTools.Web/App/Components/Permissions/AddEditUserPortalPermissions.jsx
@@ -10,7 +10,7 @@ import { connect } from 'react-redux';
     };
 })
 /// <summary>
-/// Sub component of product page to  add ,edit user basic details
+/// Sub component of user permission page to  add ,edit user portal details
 /// </summary>
 class AddEditUserPortalPermissions extends React.Component {
     /// <summary>
@@ -25,7 +25,7 @@ class AddEditUserPortalPermissions extends React.Component {
             userPortalPermissionunmodifiedModel: "",
             componentJustMounted: true
         });
-        this.activeStatusChange=this.activeStatusChange.bind(this);
+
     }
 
     componentWillMount() {
@@ -48,76 +48,70 @@ class AddEditUserPortalPermissions extends React.Component {
         }, function () {
             if (this.state.componentJustMounted) {
                 this.setState({ componentJustMounted: false }, function () {
-                  
+
                 });
             }
         });
 
     }
 
-    activeStatusChange()
-    {
-        console.log("sdafsda");
-            
-    }
 
-    rowConstruct(){
+    activechkChange(key, value, event) {
+        var model = this.state.userPortalPermissionModel;
 
-       
-    }
-    activechkChange(key,value,event)
-    {
-        var model=this.state.userPortalPermissionModel;
-            
-        model.portal.modulePermissions[key][value]=!this.state.userPortalPermissionModel.portal.modulePermissions[key][value];
-        
+        model.portal.modulePermissions[key][value] = !this.state.userPortalPermissionModel.portal.modulePermissions[key][value];
+
         this.setState({
             userPortalPermissionModel: model
         });
         this.props.updatePermission(model);
     }
+
+
     render() {
         let row = null;
-        let vals=null;
+        let vals = null;
         row = this.state.userPortalPermissionModel.portal.modulePermissions;
-        vals=Object.keys(row).map(function(key,index) {
-          
+        vals = Object.keys(row).map(function (key, index) {
+
             return (<Row componentClass="tr" key={index.toString()}>
-    
-                    <Col componentClass="td">{key}</Col>
-                    <Col componentClass="td">
-                         <input type="checkbox"  checked={row[key].canRead} onChange={(event) => this.activechkChange(key, "canRead", event)} />
-                                     </Col>
-                    <Col componentClass="td"> <input type="checkbox"  checked={row[key].canAdd} onChange={(event) => this.activechkChange(key, "canAdd", event)} /></Col>
-                    <Col componentClass="td"><input type="checkbox"  checked={row[key].canEdit} onChange={(event) => this.activechkChange(key, "canEdit", event)} /></Col>
-                     <Col componentClass="td"><input type="checkbox"  checked={row[key].canDelete} onChange={(event) => this.activechkChange(key, "canDelete", event)} /></Col>
-           
-                </Row>)
+
+                <Col componentClass="td" class="user-permission-portal-module">{key}</Col>
+                <Col componentClass="td"><input type="checkbox" checked={row[key].canRead}
+                    onChange={(event) => this.activechkChange(key, "canRead", event)} disabled={this.state.userPortalPermissionModel.portal.isAdmin ? true : false} /></Col>
+                <Col componentClass="td"> <input type="checkbox" checked={row[key].canAdd}
+                    onChange={(event) => this.activechkChange(key, "canAdd", event)} disabled={this.state.userPortalPermissionModel.portal.isAdmin ? true : false} /></Col>
+                <Col componentClass="td"><input type="checkbox" checked={row[key].canEdit}
+                    onChange={(event) => this.activechkChange(key, "canEdit", event)} disabled={this.state.userPortalPermissionModel.portal.isAdmin ? true : false} /></Col>
+                <Col componentClass="td"><input type="checkbox" checked={row[key].canDelete}
+                    onChange={(event) => this.activechkChange(key, "canDelete", event)} disabled={this.state.userPortalPermissionModel.portal.isAdmin ? true : false} /></Col>
+
+            </Row>)
         }.bind(this));
-      
+
         return (
             <div>
-               
-        <div className="clearBoth modalTableContainer">
+
+                <div className="clearBoth modalTableContainer">
                     <Grid componentClass="table" class="user-permission-portal-table" >
                         <thead>
                             <Row componentClass="tr">
-                                <Col componentClass="th"><label>Module</label></Col>
+                                <Col componentClass="th" class="user-permission-portal-module"><label>Module</label></Col>
                                 <Col componentClass="th"> <label>Read</label></Col>
                                 <Col componentClass="th"><label >Add</label></Col>
                                 <Col componentClass="th"><label>Edit</label></Col>
-                                 <Col componentClass="th"><label>Delete</label></Col>
+                                <Col componentClass="th"><label>Delete</label></Col>
                             </Row>
                         </thead>
                         <tbody>
-                    {vals}
+                            {vals}
                         </tbody>
                     </Grid>
                 </div>
-               
+
             </div>
         )
-                        }
-            }
+    }
+}
 
 export default AddEditUserPortalPermissions
