75afd653431e751474dd2f79ac3874a77e3de54e
Nirmala Subramaniam <nirmalasubu@yahoo.com>
2017-10-03 14:17:16 -0400

    ODTD-984 pop over added

--- OnDemandTools.Web/App/Components/Common/Header.jsx
+++ OnDemandTools.Web/App/Components/Common/Header.jsx
@@ -4,7 +4,7 @@ import { Image } from 'react-bootstrap';
 import { connect } from 'react-redux';
 import { fetchUser } from 'Actions/User/UserActions';
 import { fetchConfig } from 'Actions/Config/ConfigActions';
-import { ButtonToolbar, DropdownButton, MenuItem, Tabs, Tab, Grid, Row, Col, Button, } from 'react-bootstrap';
+import { ButtonToolbar, DropdownButton, MenuItem, Tabs, Tab, Grid, Row, Col, Button, OverlayTrigger, Popover } from 'react-bootstrap';
 import $ from 'jquery';
 import SessionPage from 'Components/Common/SessionHandling/SessionPage';
 
@@ -48,54 +48,63 @@ class Header extends React.Component {
         }
     }
 
+
     render() {
-        var userFullName,apiKey,lastLogin,userName = "";
-        var isAPIActive,headerLogo,version = null;
-       
+
+        var userFullName, apiKey, lastLogin, userName = "";
+        var isAPIActive,userlogo,  version = null;
+
         if (this.props.user.firstName != undefined) {
-            userFullName=  "  " + this.props.user.firstName + " " + this.props.user.lastName;
-            headerLogo = <label class="headerLogo" >
-                                <i class="fa fa-user"></i>{userFullName}
-                            </label> 
-            userName= this.props.user.userName;            
+            userFullName="  " + this.props.user.firstName + " " + this.props.user.lastName;
+            userlogo = <i class="fa fa-user"></i>;
+            userName = this.props.user.userName;
             apiKey = this.props.user.api.apiKey;
             lastLogin = this.formatDate(this.props.user.portal.lastLoginTime);
             isAPIActive = this.props.user.api.isActive ? "" : <span class="header-inactiveApi"> (Inactive)</span>;
-        }
-                if (this.props.config.version != undefined) {
-                    version=<p class="header-version">Version:{this.props.config.version}</p>
-                }
+            }
+
+        if (this.props.config.version != undefined) {
+            version = <p class="header-version">Version:{this.props.config.version}</p>
+            }
+
+        const popoverClickRootClose = (
+            <Popover id="popover-trigger-click-root-close" class="headerpopover-content">
+                <label>User Name: </label> <p>{this.props.user.userName} </p>
+                <label>Api Key: </label><p>{apiKey} {isAPIActive}  </p>
+                <label>Last Login: </label>{lastLogin}
+                <hr />
+                <a href="/account/logoff">Logout </a>
+            </Popover>
+        );
+
         return (
             <div >
                 <Grid fluid>
                     <Row>
-                         <Col md={2} lg={2} >
-                        <Image src="../images/ODTLogo.png" rounded />
+                        <Col md={2} lg={2} >
+                            <Image src="../images/ODTLogo.png" rounded />
                         </Col>
-                         <Col md={10} >
-                        <div class="header headerdropdown">
-                            <span>{headerLogo}</span>
-                            <div class="headerdropdown-content">
-                                <p><strong>User Name:</strong> {userName}</p>
-                                <p><strong>Api Key: </strong><br/>{apiKey}{isAPIActive}</p>
-                                <p><strong>Last Login:</strong> {lastLogin}</p>
-                                <hr />
-                                <a href="/account/logoff">Logout </a>
+                        <Col md={10} >
+                            <div class="header">
+                                <OverlayTrigger trigger="click" rootClose placement="bottom" overlay={popoverClickRootClose}>
+                                    <button class="btn-link" >
+                                    {userlogo}{userFullName}
+                                    </button>
+                                </OverlayTrigger>
                             </div>
-                        </div>
-                         </Col>
+                        </Col>
                     </Row>
                     <Row>
-                         <Col md={2} lg={2} />
-                         <Col md={10} >
-                            {version}
+                        <Col md={2} lg={2} />
+                        <Col md={10} >
+            {version}
                         </Col>
                     </Row>
                 </Grid >
                 <hr />
             </div>
         )
-    }
-}
+            }
+            }
 
 export default Header;
